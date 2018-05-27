52b294712f781f7beec7b2c37dd4abb13ad1e5bb
Methsaan Wick <methsaan@gmail.com>
2018-05-27 18:51:46 -0400

    update

--- usefulprograms/calculator.c
+++ usefulprograms/calculator.c
@@ -3,55 +3,25 @@
 
 int main(int argc, char* argv)
 {
-	char operator[20];
-	double a;
-	double b;
-	double c;
-	printf("Welcome to calculator. Options: add, sub, mul, div\n");
-	printf("Enter operator: ");
-	scanf("%s", operator);
-	printf("Enter first number: ");
-	scanf("%f", &a);
-	printf("Enter second number: ");
-<<<<<<< HEAD
-	scanf("%f", &b);
-	if (operator == "add"){
-		c = a + b;
-		printf("%f + %f = %f\n", a, b, c);
+	double x;
+	double y;
+	char op[1];
+	printf("Enter operator:(+, -, x, /) ");
+	scanf("%s", op);
+	printf("Enter x: ");
+	scanf("%f", &x);
+	printf("Enter y: ");
+	scanf("%f", &y);
+	if (op == "+"){
+		printf("%f + %f = %f\n", x, y, x+y);
 	}
-	if (operator == "sub"){
-		c = a - b;
-		printf("%f - %f = %f\n", a, b, c);
+	if (op == "-"){
+		printf("%f - %f = %f\n", x, y, x-y);
 	}
-	if (operator == "mul"){
-		c = a * b;
-		printf("%f x %f = %f\n", a, b, c);
+	if (op == "x"){
+		printf("%f x %f = %f\n", x, y, x*y);
 	}
-	if (operator == "div"){
-		c = a / b;
-		printf("%f / %f = %f\n", a, b, c);
+	if (op == "/"){
+		printf("%f / %f = %f\n", x, y, x/y);
 	}
-	if (operator != "add") && (operator != "sub") && (operator != "mul") && (operator != "div"){
-		printf("Invalid operator\n");
-	}
-	return(0);
-=======
-	scanf("%lf", &b);
-	//if (operator == "add"){
-	c = a + b;
-	printf("%lf + %lf = %lf\n", a, b, c);
-	//}else if (operator == "sub"){
-	//	c = a - b;
-	//	printf("%lf - %lf = %lf\n", a, b, c);
-	//}else if (operator == "mul"){
-	//	c = a * b;
-	//	printf("%lf x %lf = %lf\n", a, b, c);
-	//}else if (operator == "div"){
-	//	c = a / b;
-	//	printf("%lf / %lf = %lf\n", a, b, c);
-	//}else{
-	//	printf("Invalid operator\n");
-	//}
-	//return(0);
->>>>>>> dd6143908f8ef7ffcdabd1a0524492eefa1fd562
 }
