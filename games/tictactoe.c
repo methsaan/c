1a3654949651d7f7bd218d82eb97d9ce371c55c4
Methsaan <methsaan@gmail.com>
2018-06-08 18:40:10 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -4,9 +4,13 @@
 #include<stdlib.h>
 
 int main() {
+	int randmax;
+	int rps2;
 	char rps[1];
 	printf("Rock, paper, or scissor (r, p, s): ");
 	scanf("%s", rps);
-	int r = rand() % 3;
-	printf("%d\n", r);
+	printf("%s\n", rps);
+	srand(time(NULL));
+	randmax = (100*rand())/RAND_MAX;
+	printf("%d %d\n", randmax, rand());
 }
