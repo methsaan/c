132af03221655e54e701df946ce1b8cad280cae0
Methsaan <methsaan@gmail.com>
2018-06-12 18:47:16 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -9,16 +9,22 @@ int main() {
 	printf("Rock, paper, or scissor (r, p, s): ");
 	scanf("%s", usrrps);
 	srand(time(NULL));
-	char rps[3][1] = {"r","p","s"};
+	char rps[3][2] = {"rr","pp","ss"};
 	randnum = rand()%3;
 	if (strcmp(usrrps, rps) == 0){
 		printf("TIE!!\n");
-	}else if ((strcmp(usrrps, 'p') == 0) && (strcmp(rps, "s") == 0)){
-		printf("I WIN!!\n");
-	}else if ((strcmp(usrrps, 's') == 0) && (strcmp(rps, "r") == 0)) {
-		printf("I WIN!!\n");
-	}else if ((strcmp(usrrps, 'r') == 0) && (strcmp(rps, "p") == 0)){
-		printf("I WIN!!\n");
+	}else if (strcmp(usrrps, "p") == 0) {
+		if (strcmp(rps, "ss") == 0) {
+			printf("I WIN!!\n");
+		}
+	}else if (strcmp(usrrps, "s") == 0){
+		if (strcmp(rps, "rr") == 0) {
+			printf("I WIN!!\n");
+		}
+	}else if (strcmp(usrrps, "r") == 0){
+		if (strcmp(rps, "pp") == 0){
+			printf("I WIN!!\n");
+		}
 	}else{
 		printf("YOU WIN!!\n");
 	}
