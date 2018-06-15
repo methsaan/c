8f24aac07b6d2caf313315ee70e2b3e3bcd48bb7
Methsaan <methsaan@gmail.com>
2018-06-15 07:35:06 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -5,23 +5,23 @@
 
 int main() {
 	int randnum;
-	char usrrps[1];
-	printf("Rock, paper, or scissor (r, p, s): ");
-	scanf("%s", usrrps);
+	const char usrrps[20];
+	printf("Type rock, paper, or scissor: ");
+	scanf("%s", &usrrps);
 	srand(time(NULL));
-	char rps[3][2] = {"rr","pp","ss"};
+	const char *rps[3][2] = {"rr","pp","ss"};
 	randnum = rand()%3;
 	if (strcmp(usrrps, rps) == 0){
 		printf("TIE!!\n");
-	}else if (strcmp(usrrps, "p") == 0) {
+	}else if (strcmp(usrrps, "paper") == 0) {
 		if (strcmp(rps, "ss") == 0) {
 			printf("I WIN!!\n");
 		}
-	}else if (strcmp(usrrps, "s") == 0){
+	}else if (strcmp(usrrps, "scissor") == 0){
 		if (strcmp(rps, "rr") == 0) {
 			printf("I WIN!!\n");
 		}
-	}else if (strcmp(usrrps, "r") == 0){
+	}else if (strcmp(usrrps, "rock") == 0){
 		if (strcmp(rps, "pp") == 0){
 			printf("I WIN!!\n");
 		}
