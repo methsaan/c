aa5ff120061e1e485baddd5374490fa23e5750ce
Methsaan <methsaan@gmail.com>
2018-06-15 07:55:23 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -4,28 +4,29 @@
 #include<stdlib.h>
 
 int main() {
-	int randnum;
-	const char usrrps[20];
+	char randw;
+	char usrrps[20];
 	printf("Type rock, paper, or scissor: ");
-	scanf("%s", &usrrps);
+	scanf("%s", usrrps);
 	srand(time(NULL));
-	const char *rps[3][2] = {"rr","pp","ss"};
-	randnum = rand()%3;
-	if (strcmp(usrrps, rps) == 0){
+	char rps[3][20] = {"rock","paper","scissor"};
+	randw = rps[rand()%3];
+	printf("%s", randw);
+	/*if (usrrps == randw){
 		printf("TIE!!\n");
-	}else if (strcmp(usrrps, "paper") == 0) {
-		if (strcmp(rps, "ss") == 0) {
+	}else if (usrrps == "paper") {
+		if (randw == "scissor") {
 			printf("I WIN!!\n");
 		}
-	}else if (strcmp(usrrps, "scissor") == 0){
-		if (strcmp(rps, "rr") == 0) {
+	}else if (usrrps == "scissor"){
+		if (randw == "rock") {
 			printf("I WIN!!\n");
 		}
-	}else if (strcmp(usrrps, "rock") == 0){
-		if (strcmp(rps, "pp") == 0){
+	}else if (usrrps == "rock"){
+		if (randw == "paper"){
 			printf("I WIN!!\n");
 		}
 	}else{
 		printf("YOU WIN!!\n");
-	}
+	}*/
 }
