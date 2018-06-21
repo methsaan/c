b338a0460ad7224005e0834185a84eb31e46a194
Methsaan <methsaan@gmail.com>
2018-06-21 07:38:46 -0400

    update

---
+++ usefulprograms/booksched.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argv, char *argc){
+	printf("________________________________________\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|       |       |       |       |       |\n");
+	printf("| DAY 1 | DAY 2 | DAY 3 | DAY 4 | DAY 5 |\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|_______|_______|_______|_______|_______|\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|  Math |English|Science|   SS  | French|\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|_______|_______|_______|_______|_______|\n");
+	printf("|       |       |       |       |       |\n");
+	printf("|La lege|Le secr|Joyeux |Fantast|Harry P|\n");
+	printf("|nd D'An|et des |Anniver|ique ma|otter a|\n");
+	printf("|gelino |Patins |saire C|itre re|nd the |\n");
+	printf("|Brown  |D'argen|atastro|nard   |Chamber|\n");
+	printf("|_______|t______|phe!___|_______|of secrets|\n");
+	printf("|_______________________________________|\n");
+}
