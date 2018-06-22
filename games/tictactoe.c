32401233a20d7f5366c388dda247a63b6181709e
Methsaan <methsaan@gmail.com>
2018-06-22 07:46:27 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -31,8 +31,6 @@ int main(int argc, char *argv) {
 		bool Stor = (strcmp(usrrps, "scissor")) && (strcmp(randw, "rock"));
 		if (((strcmp(usrrps, "rock")) && (strcmp(randw, "paper"))) || ((strcmp(usrrps, "rock")) && (strcmp(randw, "paper"))) ||  ((strcmp(usrrps, "rock")) && (strcmp(randw, "paper")))) {
 			printf("I win.\n");
-		}else if ((strcmp(usrrps, "rock") != 0) || (strcmp(usrrps, "scissor") != 0) || (strcmp(usrrps, "paper") != 0) || (strcmp(randw, "rock") != 0) || (strcmp(randw, "scissor") != 0) || (strcmp(randw, "scissor") != 0)) {
-			printf("DRAW\n");
 		}else{
 			printf("You win.\n");
 		}
