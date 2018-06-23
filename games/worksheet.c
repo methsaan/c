43305f721a716b7784e58a068302e6df94701b5b
Methsaan <methsaan@gmail.com>
2018-06-22 20:54:05 -0400

    update

--- games/worksheet.c
+++ games/worksheet.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<stdbool.h>
 
 int main(int argc, char *argv){
 	srand(time(NULL));
-	while (true){
+	while (true) {
 		printf("%d\n", (rand()%1000)+1);
 	}
 }
