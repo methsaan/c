c4b4932b17aa3ee64c8a53ad33877aa286c15a7f
Methsaan <methsaan@gmail.com>
2018-06-22 20:56:26 -0400

    update

--- games/worksheet.c
+++ games/worksheet.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv){
 	srand(time(NULL));
-	for (int x = 0; x>10; x++) {
+	for (int x = 0; x<10; x++) {
 		printf("%d X %d\n", (rand()%100000)+1, (rand()%100000)+1);
 	}
 }
