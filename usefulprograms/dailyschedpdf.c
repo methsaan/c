49a53b5cf1d17b4e0c01a28cc3bab8501dc83312
Methsaan <methsaan@gmail.com>
2018-06-24 17:37:10 -0400

    update

--- usefulprograms/dailyschedpdf.c
+++ usefulprograms/dailyschedpdf.c
@@ -13,20 +13,20 @@ int main(int argv, char *argc){
 	printf("|     D    |JavaScript/C|    FT      |     PT     |    D     |  Read      |  EQAO     |        PT       |\n");
 	printf("|     PP   |  PT        |    EQAO    |     D      |    PP    |  CPP       |  D        |        EQAO     |\n");
 	printf("|Python/Java| PP        |    PP      |     PP     |    C/Java|  PP        |  PP       | Python/Java/C++/|\n");
-	printf("|  PT      |  PA        |    PT      | JavaScript |    PT    |  program   |HTML/JS/C++|    C/JS/HTML    |\n");
+	printf("|  PT      |  PA        |    PT      | JavaScript |    PT    |  program   |HTML/JS/C++|    C/JS/HTML 1  |\n");
 	printf("|  PA      |  FT        | HTML/C++   |     PA     |    PA    |  FT        | PT        |Smart books/Read |\n");
 	printf("|__________|____________|____________|____________|__________|  Java      | FT        |       FT        |\n");
 	printf("                                                             |  PP        | PP        |       PA        |\n");
-	printf("                      Sinhala dictation 5:30pm               |  PBT       | D         |       PP        |\n");
+	printf("                      Sinhala dictation 5:30pm               |Smart books | D         |       PP        |\n");
 	printf("                        PA = physical activity               |  FT        | FT        |       Read      |\n");
-	printf("                        PT = piano theory                    |            |           |       PT        |\n");
-	printf("                        PP = practical piano                 |            |           |       FT        |\n");
+	printf("                        PT = piano theory                    |            | PP        |       PT        |\n");
+	printf("                        PP = practical piano                 |            | C         |       FT        |\n");
 	printf("                        D = duolingo                         |            |           |       D         |\n");
 	printf("                        FT = free time                       |____________|___________|_________________|\n");
 	printf("                        PBT = Programming Blog tutorial                                                  \n");
 	printf("                        Monday: wake up 5:30 sleep 9:15                                                  \n");
-	printf("                        Tuesday: wake up 5:30 sleep 9:15        Saturday: wake up 5:30 sleep 9:45        \n");
-	printf("                        Wednesday: wake up 5:30 sleep 9:15      Sunday: wake up 5:30 sleep 9:45          \n");
+	printf("                        Tuesday: wake up 5:30 sleep 9:15        Saturday: wake up 5:30 sleep 9:30        \n");
+	printf("                        Wednesday: wake up 5:30 sleep 9:15      Sunday: wake up 5:30 sleep 9:30          \n");
 	printf("                        Thursday: wake up 5:30 sleep 9:15       PD day: wake up 5:30 sleep 9:00          \n");
 	printf("                        Friday: wake up 5:30 sleep 9:45                                                  \n");
 }
