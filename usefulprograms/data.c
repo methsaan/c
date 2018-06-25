b1471fa75e9b99a8de15656dadd129f0a08d8043
Methsaan <methsaan@gmail.com>
2018-06-25 07:59:29 -0400

    update

--- usefulprograms/data.c
+++ usefulprograms/data.c
@@ -1,26 +1,36 @@
 #include<stdio.h>
 #include<string.h>
-#include<array>
 
+static int cmp(const void *p1, const void *p2){
+	return *(int*)p1 > *(int*)p2;
+}
 int main(int argv, char *argc){
 	char type[20];
 	int numOfData;
 	printf("Enter mean, median, mode or range: ");
 	scanf("%s", type);
 	printf("Enter number of data: ");
-	scanf("%d", numOfData);
+	scanf("%d", &numOfData);
 	int data[numOfData];
 	for (int x = 0; x < numOfData; x++){
 		int y;
 		printf("Enter data: ");
-		scanf("%d", y);
+		scanf("%d", &y);
 		data[x] = y;
 	}
-	if (type == "mean"){
-		int sum = 0;
+	if (strcmp(type, "mean") == 0){
+		double sum = 0.00;
 		for (int x = 0; x < numOfData; x++){
 			sum += data[x];
 		}
-		printf("%f\n", sum/numOfData);
+		printf("%.2f\n", sum/numOfData);
+	}else if (strcmp(type, "median") == 0){
+		if (numOfData%2 == 0){
+			double middle = ((numOfData/2)+((numOfData/2)+1))/2;
+		}else {
+			qsort(data, sizeof(data)/sizeof(int), cmp);
+			double middle = (numOfData/2)+0.5;
+		}
+
 	}
 }
