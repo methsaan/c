1abebf1ab80c2db1f728129422b7685a0e70dfea
Methsaan <methsaan@gmail.com>
2018-06-25 06:42:59 -0400

    update

--- games/tictactoe.c
+++ games/tictactoe.c
@@ -11,25 +11,24 @@ int main(int argc, char *argv) {
 	srand(time(NULL));
 	int twelveRand = (rand()%12)+1;
 	char rps[3][20];
-	if ((twelveRand == 1) || (twelveRand == 4) || (twelveRand == 7) || (twelveRand == 10)) {
+	if ((twelveRand == 1) || (twelveRand == 3) || (twelveRand == 8) || (twelveRand == 12)) {
 		strcpy(rps[0], "rock");
 	}
-	else if ((twelveRand == 2) || (twelveRand == 5) || (twelveRand == 8) || (twelveRand == 11)){
+	else if ((twelveRand == 9) || (twelveRand == 5) || (twelveRand == 4) || (twelveRand == 11)){
 		strcpy(rps[1], "paper");
 	}
 	else {
 		strcpy(rps[2], "scissor");
 	}
 	char randw[20];
-	strcpy(randw, rps[rand()%3]);
-	printf("Computer choice: %s. Your choice: %s", randw, usrrps);
+	printf("%s\n", rps[rand()%3]);
+	randw = rps[rand()%3];
+	printf("%s\n", randw);
+	printf("Computer choice: %s.  Your choice: %s\n", randw, usrrps);
 	if (strcmp(usrrps, randw) == 0){
 		printf("TIE\n");
 	}else{
-		bool Rtop = (strcmp(usrrps, "rock")) && (strcmp(randw, "paper"));
-		bool Ptos = (strcmp(usrrps, "paper")) && (strcmp(randw, "scissor"));
-		bool Stor = (strcmp(usrrps, "scissor")) && (strcmp(randw, "rock"));
-		if (((strcmp(usrrps, "rock")) && (strcmp(randw, "paper"))) || ((strcmp(usrrps, "rock")) && (strcmp(randw, "paper"))) ||  ((strcmp(usrrps, "rock")) && (strcmp(randw, "paper")))) {
+		if (((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0)) || ((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0)) ||  ((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0))) {
 			printf("I win.\n");
 		}else{
 			printf("You win.\n");
