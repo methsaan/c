ebaa100c1044dddbb6a8d38bb3415e40e7c6eeaa
Methsaan <methsaan@gmail.com>
2018-06-24 21:46:48 -0400

    update

--- games/worksheet.c
+++ games/worksheet.c
@@ -9,4 +9,10 @@ int main(int argc, char *argv){
 		printf("%d x %d\n", (rand()%100000)+1, (rand()%100000)+1);
 		printf("%d \\ %d\n", (rand()%10000)+1, (rand()%1000)+1);
 	}
+	for (int x = 0; x<7; x++) {
+		printf("Radius of circle is  %f. Find area and perimeter\n", (rand()%1000)/100);
+	}
+	for (int x = 0; x<5; x++){
+		printf("Radius of cylinder is %f Height is %f. Find surface area and volume\n", (rand()%1000)/100, (rand()%10000/100));
+	}
 }
