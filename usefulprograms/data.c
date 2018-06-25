25a23a1391a07726c5c76bbfc7c231e5eb4360d5
Methsaan <methsaan@gmail.com>
2018-06-25 07:32:10 -0400

    update

--- usefulprograms/data.c
+++ usefulprograms/data.c
@@ -5,7 +5,22 @@
 int main(int argv, char *argc){
 	char type[20];
 	int numOfData;
-	printf("mean, median, mode or range: ");
+	printf("Enter mean, median, mode or range: ");
 	scanf("%s", type);
-	printf("");
+	printf("Enter number of data: ");
+	scanf("%d", numOfData);
+	int data[numOfData];
+	for (int x = 0; x < numOfData; x++){
+		int y;
+		printf("Enter data: ");
+		scanf("%d", y);
+		data[x] = y;
+	}
+	if (type == "mean"){
+		int sum = 0;
+		for (int x = 0; x < numOfData; x++){
+			sum += data[x];
+		}
+		printf("%f\n", sum/numOfData);
+	}
 }
