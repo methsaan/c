37e0a218dd1508e30c540a74f7c3a8e97ce6ae86
Methsaan <methsaan@gmail.com>
2018-06-25 07:05:00 -0400

    update

---
+++ usefulprograms/data.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<array>
+
+int main(int argv, char *argc){
+	char type[20];
+	int numOfData;
+	printf("mean, median, mode or range: ");
+	scanf("%s", type);
+	printf("");
+}
