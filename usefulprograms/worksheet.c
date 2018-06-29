715a6c9f22d159cbc51d337b0051d5ef51a4cba1
Methsaan <methsaan@gmail.com>
2018-06-29 11:31:15 -0400

    update

--- usefulprograms/worksheet.c
+++ usefulprograms/worksheet.c
@@ -5,14 +5,53 @@
 
 int main(int argc, char *argv){
 	srand(time(NULL));
-	for (int x = 0; x<10; x++) {
-		printf("%d x %d\n", (rand()%100000)+1, (rand()%100000)+1);
-		printf("%d \\ %d\n", (rand()%10000)+1, (rand()%1000)+1);
+	double ans;
+	int ans2;
+	int score;
+	for (int x = 0; x<6; x++) {
+		int xmul = (rand()%10000)+1;
+		int ymul = (rand()%10000)+1;
+		printf("%d x %d ", xmul, ymul);
+		scanf("%d", &ans2);
+		if (xmul*ymul == ans2){
+			score++;
+		}
+		double xdiv = (rand()%10000)+1;
+		double ydiv = (rand()%1000)+1;
+		printf("%f / %f ", xdiv, ydiv);
+		scanf("%f", &ans);
+		if (xdiv/ydiv == ans){
+			score++;
+		}
 	}
-	for (int x = 0; x<7; x++) {
-		printf("Radius of circle is  %f. Find area and perimeter\n", (rand()%1000)/100.00);
+	for (int x = 0; x<5; x++) {
+		double rad = (rand()%1000)/100.00;
+		printf("Radius of circle is  %f", rad);
+		printf("Enter circumference: ");
+		scanf("%f", &ans);
+		if (2*rad*3.14 == ans){
+			score++;
+		}
+		printf("Enter area: ");
+		scanf("%f", &ans);
+		if (3.14*rad*rad == ans){
+			score++;
+		}
 	}
-	for (int x = 0; x<5; x++){
-		printf("Radius of cylinder is %f Height is %f. Find surface area and volume\n", (rand()%1000)/100.00, (rand()%10000/100.00));
+	for (int x = 0; x<4; x++){
+		double rad = (rand()%1000)/100.00;
+		double h = (rand()%10000)/100.00;
+		printf("Radius of cylinder is %f Height is %f.", rad, h);
+		printf("Enter surface area: ");
+		scanf("%f", &ans);
+		if (((2*rad*3.14)*h)+((3.14*rad*rad)*2) == ans){
+			score++;
+		}
+		printf("Enter volume: ");
+		scanf("%f", &ans);
+		if ((3.14*rad*rad)*h == ans){
+			score++;
+		}
 	}
+	printf("%d", score);
 }
