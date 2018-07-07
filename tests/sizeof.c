bb2c9247e7d6b13cd635ff1597d286db89b5e7b3
Methsaan <methsaan@gmail.com>
2018-07-07 08:22:13 -0400

    update

--- tests/sizeof.c
+++ tests/sizeof.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include<unistd.h>
 
+int arrsize(double * arr){
+	return sizeof(arr)/sizeof(*arr);
+}
 int main(int argc, char * argv){
 	double x[] = {12.13, 2356.125, 123.21512, 2133.2512, 1234.5, 264.12, 1242.5};
-	printf("%d\n", sizeof(x)/sizeof(*x));
+	printf("%d\n", arrsize(x));
 }
