f31f22948d9ff101ce48e5a586b06f948c02265b
Methsaan <methsaan@gmail.com>
2018-07-08 12:12:25 -0400

    update

--- tests/sizeof.c
+++ tests/sizeof.c
@@ -4,6 +4,7 @@
 int arrsize(double * arr){
 	return sizeof(arr)/sizeof(*arr);
 }
+
 int main(int argc, char * argv){
 	double x[] = {12.13, 2356.125, 123.21512, 2133.2512, 1234.5, 264.12, 1242.5};
 	printf("%d\n", arrsize(x));
