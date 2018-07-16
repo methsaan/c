74b6f3b277251bdd1ca03e48aee9715404d92dad
Methsaan <methsaan@gmail.com>
2018-07-16 16:53:42 -0400

    update

---
+++ usefulprograms/multalg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc, char *argv){
+	int f;
+	int s;
+	printf("Enter first num: ");
+	scanf("%d", &f);
+	printf("Enter second num: ");
+	scanf("%d", &s);
+	printf("    %d\n", f);
+	printf("    %d\n", s);
+	printf("   ______\n");
+	printf("   ");
+}
