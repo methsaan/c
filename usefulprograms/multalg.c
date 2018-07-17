8aa9613014c51f68c68bdb7dde77c548c5b9a2fc
Methsaan <methsaan@gmail.com>
2018-07-17 08:53:19 -0400

    update

--- usefulprograms/multalg.c
+++ usefulprograms/multalg.c
@@ -3,12 +3,20 @@
 int main(int argc, char *argv){
 	int f;
 	int s;
-	printf("Enter first num: ");
+	char strf[5];
+	char strs[5];
+	printf("Enter first num (2 digits): ");
 	scanf("%d", &f);
-	printf("Enter second num: ");
+	printf("Enter second num (2 digits): ");
 	scanf("%d", &s);
 	printf("    %d\n", f);
 	printf("    %d\n", s);
 	printf("   ______\n");
-	printf("   ");
+	strf = itoa(f, strf, 10);
+	int prod1 = atoi(strs[1])*f;
+	int prod2 = atoi(strs[0])*f;
+	printf("   %d\n", prod1);
+	printf("  %d\n", prod2);
+	printf("_________\n");
+	printf("%d", prod1+prod2);
 }
