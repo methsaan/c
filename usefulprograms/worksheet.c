e3c767e0a47881753aba695202e20138f4be68cf
Methsaan <methsaan@gmail.com>
2018-07-22 19:04:11 -0400

    pudate

--- usefulprograms/worksheet.c
+++ usefulprograms/worksheet.c
@@ -7,15 +7,19 @@
 
 int main(int argc, char *argv){
 	srand(time(NULL));
-	int x = (rand()%100)+1;
-	int y = (rand()%100)+1;
-	int inp = x+y;
-	printf("What is %d + %d? ", x, y);
-	scanf("%d", &inp);
-	if (inp == x+y){
-		printf("Correct\n");
-	}else{
-		printf("wrong %d", x+y);
-	}
+	int score = 0;
+	for (int a = 0; a < 10; a++){
+		int x = (rand()%100)+1;
+		int y = (rand()%100)+1;
+		int inp = x+y;
+		printf("What is %d x %d? ", x, y);
+		scanf("%d", &inp);
+		if (inp == x*y){
+			score++;
+		}else{
+			score++;
+			score--;
+		}
+	{
 	return 0;
 }
