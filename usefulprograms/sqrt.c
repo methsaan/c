923b27af89070e8346529bd64847f442b2954ad9
Methsaan <methsaan@gmail.com>
2018-07-24 07:04:10 -0400

    update

--- usefulprograms/sqrt.c
+++ usefulprograms/sqrt.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+#include<math.h>
 
-double Sqrt(int A, int B, int C, int Bsqrt){
+double formulaSqrt(int A, int B, int C, int Bsqrt){
 	return (1.00 / (A - B)) * (C - B) + Bsqrt;
 }
 int main(int argc, char *argv){
@@ -10,11 +11,12 @@ int main(int argc, char *argv){
         int bsqrt;
 	printf("Enter C: ");
 	scanf("%d", &c);
-	printf("Enter closest square num before C (stored as B): ");
+	printf("Enter closest square num before C: (stored as B) ");
 	scanf("%d", &b);
-	printf("Enter closest square num after C: (stored as A): ");
+	printf("Enter closest square num after C: (stored as A) ");
 	scanf("%d", &a);
 	printf("Enter square root of B: ");
 	scanf("%d", &bsqrt);
-	printf("Square root of C is aproximately %f\n", Sqrt(a, b, c, bsqrt));
+	printf("Square root according to formula: %f\n", formulaSqrt(a, b, c, bsqrt));
+	printf("Square root according to sqrt(): %f\n", sqrt(c));
 }
