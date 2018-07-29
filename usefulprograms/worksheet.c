79357e612fbe8d9b41268badb67808b773eec738
Methsaan <methsaan@gmail.com>
2018-07-29 15:16:26 -0400

    update

--- usefulprograms/worksheet.c
+++ usefulprograms/worksheet.c
@@ -17,20 +17,21 @@ int main(int argc, char *argv){
 		system("clear");
 		int x = (rand()%1000000)+1;
 		int y = (rand()%1000000)+1;
-		int inp;
+		long int inp;
 		printf("What is %d x %d? ", x, y);
-		scanf("%d", &inp);
+		scanf("%ld", &inp);
 		if (inp == x*y) {
 			score++;
 			pScore = &score;
 		}else{
+			printf("Your answer: %ld\ncorrect answer: %ld\n", inp, x*y);
 			pScore = &score;
 		}
 		//system("clear");
 		//float xd = (rand()%1000)+1.0;
 		//float yd = (rand()%10)+1.0;
 		//float inp2;
-		//printf("What is %f / %f? (2 decimals) ", xd, yd);
+		//printf("What is %g / %g? (2 decimals) ", xd, yd);
 		//scanf("%f", inp2);
 		//printf("%f\n", Round(xd/yd));
 	}
