6419c2581e386810d454c87b0d052a411d7968ca
Methsaan <methsaan@gmail.com>
2018-08-19 14:05:05 -0400

    update

---
+++ programs/sign.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(int argc, char *argv){
+	char sym[] = "u00C0: \u00C0    u00f0: \u00f0    u00D0: \u00D0    \u00e0    \u00b0    \u00C1    \u00C2    \u00C3    \u00C4    \u00C5    \u00C6    \u00C7    \u00C8    \u00C9    \u00F1    \u00F2    \u00F3    \u00F4    \u00F5    \u00F6    \u00F7    \u00F8    \u00F9    \u00D1    \u00D2    \u00D3    \u00D4    \u00D5    \u00D6    \u00D7    \u00D8    \u00D9    \u00B1    \u00B2    \u00B3    \u00B4    \u00B5    \u00B6    \u00B7    \u00B8    \u00B9    \u00E1    \u00E2    \u00E3    \u00E4    \u00E5    \u00E6    \u00E7    \u00E8    \u00E9    \u00Fd    \u00Fb    \u00Fc    \u00Ff    \u00Fe    \u00Dd    \u00Db    \u00Dc    \u00Df    \u00De    \u00Bd    \u00Bb    \u00Bc    \u00Bf    \u00Be    \u00Cd    \u00Cb    \u00Cc    \u00Cf    \u00Ce    \u00Ed    \u00Eb    \u00Ec    \u00Ef    \u00Ee";
+	printf("\n %s \n\n", sym);
+}
