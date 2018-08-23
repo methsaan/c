ebbb104b932c678a98faf121e0f4ccce83236a33
Methsaan <methsaan@gmail.com>
2018-08-22 21:02:32 -0400

    update

--- games/rps.c
+++ games/rps.c
@@ -5,32 +5,11 @@
 #include<stdbool.h>
 
 int main(int argc, char *argv) {
-	char usrrps[20];
-	printf("Type rock, paper, or scissor: ");
-	scanf("%s", usrrps);
-	srand(time(NULL));
-	int twelveRand = (rand()%12)+1;
-	char rps[3][20];
-	if ((twelveRand == 1) || (twelveRand == 3) || (twelveRand == 8) || (twelveRand == 12)) {
-		strcpy(rps[0], "rock");
-	}
-	else if ((twelveRand == 9) || (twelveRand == 5) || (twelveRand == 4) || (twelveRand == 11)){
-		strcpy(rps[1], "paper");
-	}
-	else {
-		strcpy(rps[2], "scissor");
-	}
-	char randw[20];
-	strcpy(randw, rps[rand()%3]);
-	printf("%s\n", randw);
-	printf("Computer choice: %s.  Your choice: %s\n", randw, usrrps);
-	if (strcmp(usrrps, randw) == 0){
-		printf("TIE\n");
-	}else{
-		if (((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0)) || ((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0)) ||  ((strcmp(usrrps, "rock") == 0) && (strcmp(randw, "paper") == 0))) {
-			printf("I win.\n");
-		}else{
-			printf("You win.\n");
-		}
-	}
+	int x = (rand()%3)+1;
+	char *arr1 = "sc";
+	char *arr2 = "pa";
+	char *arr3 = "ro";
+	char arr[20] = {&arr1, &pa, &ro};
+	char *CMPchoice = arr[x];
+	puts(CMPchoice);
 }
--- usefulprograms/data.c
+++ usefulprograms/data.c
@@ -51,88 +51,86 @@ int main(int argv, char *argc){
 			sum += data[x];
 		}
 		printf("Mean: %g\n", sum/numOfData);
-	}else if (strcmp(type, "median") == 0){
-		while (1){
-			int swapped = 0;
+	}else if(strcmp(type, "median") == 0){
+		while (1){																			int swapped = 0;
 			for (int x = 0; x < numOfData-1; x++){
 				if (data[x] > data[x+1]){
 					int temp = data[x];
 					data[x] = data[x+1];
-					data[x+1] = temp;
-					swapped = 1;
+					data[x+1] = temp;																	swapped = 1;
+				}
+				if (swapped == 0){
+					break;
 				}
 			}
-			if (swapped == 0){
-				break;
-			}
-		}
-		if (numOfData%2 == 0){
-			int midsum = data[(numOfData/2)-1]+data[numOfData/2];
-			double mid = midsum/2.00;
-			printf("Median: %g\n", mid);
-		}else {
-			int a = (numOfData/2)+0.5;
-			printf("Median: %d\n", data[a]);
-		}
-	}else if (strcmp(type, "mode") == 0){
-		int freqPairs[numOfData*2];
-		int i, j, count, freq[numOfData];
-		for (i = 0; i < numOfData; i++){
-			freq[i] = -1;
-		}
-		for (i = 0; i < numOfData; i++){
-			count = 1;
-			for (j=i+1; j<numOfData; j++){
-				if (data[i] == data[j]){
-					count++;
-					freq[j] = 0;
-				}
-			}
-			if (freq[i] != 0){
-				freq[i] = count;
-			}
-		}
-		for (i = 0; i<numOfData; i++){
-			if (freq[i] != 0){
-				printf("%d occurs %d times\n", data[i], freq[i]);
-				freqPairs[i] = data[i];
-				printf("freqPairs[%d] = %d\n", i, freqPairs[i]);
-				freqPairs[i+1] = freq[i];
-				printf("freqPairs[%d] = %d\n", i+1, freqPairs[i+1]);
-			}
-		}
-		for (int b = 0; b < numOfData*2;b++){
-			printf("%d ", freqPairs[b]);
-		}
-		printf("\n");
-		printf("freq: ");
-		for (int x = 0; x < numOfData; x++){
-			printf("%d ", freq[x]);
-		}
-		printf("\n");
-		int c, largest;
-		largest = freq[0];
-		for (c = 1; c < numOfData; c++){
-			if (largest < freq[c]){
-				largest = freq[c];
-			}
-		}
-		printf("Mode: %d\n", freqPairs[largest+1]);
-	}else if (strcmp(type, "range") == 0){
-		while (1) {
-			int swapped = 0;
-			for (int x = 0; x < numOfData-1; x++){
-				if (data[x] > data[x+1]){
-					int temp = data[x];
-					data[x] = data[x+1];
-					data[x+1] = temp;
-					swapped = 1;
-				}
-			}
-			if (swapped == 0){
-				break;
-			}
-		}
-		printf("Range: %d\n", data[numOfData-1]-data[0]);
-	}
-}
+			if (numOfData%2 == 0){
+				int midsum = data[(numOfData/2)-1]+data[numOfData/2];
+				double mid = midsum/2.00;
+//																																																																																										printf("Median: %g\n", mid);
+//																																																																																												}else {
+//																																																																																															int a = (numOfData/2)+0.5;
+//																																																																																																		printf("Median: %d\n", data[a]);
+//																																																																																																				}
+//																																																																																																					}else if (strcmp(type, "mode") == 0){
+//																																																																																																							int freqPairs[numOfData*2];
+//																																																																																																									int i, j, count, freq[numOfData];
+//																																																																																																											for (i = 0; i < numOfData; i++){
+//																																																																																																														freq[i] = -1;
+//																																																																																																																}
+//																																																																																																																		for (i = 0; i < numOfData; i++){
+//																																																																																																																					count = 1;
+//																																																																																																																								for (j=i+1; j<numOfData; j++){
+//																																																																																																																												if (data[i] == data[j]){
+//																																																																																																																																	count++;
+//																																																																																																																																						freq[j] = 0;
+//																																																																																																																																										}
+//																																																																																																																																													}
+//																																																																																																																																																if (freq[i] != 0){
+//																																																																																																																																																				freq[i] = count;
+//																																																																																																																																																							}
+//																																																																																																																																																									}
+//																																																																																																																																																											for (i = 0; i<numOfData; i++){
+//																																																																																																																																																														if (freq[i] != 0){
+//																																																																																																																																																																		printf("%d occurs %d times\n", data[i], freq[i]);
+//																																																																																																																																																																						freqPairs[i] = data[i];
+//																																																																																																																																																																										printf("freqPairs[%d] = %d\n", i, freqPairs[i]);
+//																																																																																																																																																																														freqPairs[i+1] = freq[i];
+//																																																																																																																																																																																		printf("freqPairs[%d] = %d\n", i+1, freqPairs[i+1]);
+//																																																																																																																																																																																					}
+//																																																																																																																																																																																							}
+//																																																																																																																																																																																									for (int b = 0; b < numOfData*2;b++){
+//																																																																																																																																																																																												printf("%d ", freqPairs[b]);
+//																																																																																																																																																																																														}
+//																																																																																																																																																																																																printf("\n");
+//																																																																																																																																																																																																		printf("freq: ");
+//																																																																																																																																																																																																				for (int x = 0; x < numOfData; x++){
+//																																																																																																																																																																																																							printf("%d ", freq[x]);
+//																																																																																																																																																																																																										}
+//																																																																																																																																																																																																											printf("\n");
+//																																																																																																																																																																																																													int c, largest;
+//																																																																																																																																																																																																															largest = freq[0];
+//																																																																																																																																																																																																																	for (c = 1; c < numOfData; c++){
+//																																																																																																																																																																																																																				if (largest < freq[c]){
+//																																																																																																																																																																																																																								largest = freq[c];
+//																																																																																																																																																																																																																											}
+//																																																																																																																																																																																																																													}
+//																																																																																																																																																																																																																															printf("Mode: %d\n", freqPairs[largest+1]);
+//																																																																																																																																																																																																																																}else if (strcmp(type, "range") == 0){
+//																																																																																																																																																																																																																																		while (1) {
+//																																																																																																																																																																																																																																					int swapped = 0;
+//																																																																																																																																																																																																																																								for (int x = 0; x < numOfData-1; x++){
+//																																																																																																																																																																																																																																												if (data[x] > data[x+1]){
+//																																																																																																																																																																																																																																																	int temp = data[x];
+//																																																																																																																																																																																																																																																						data[x] = data[x+1];
+//																																																																																																																																																																																																																																																											data[x+1] = temp;
+//																																																																																																																																																																																																																																																																swapped = 1;
+//																																																																																																																																																																																																																																																																				}
+//																																																																																																																																																																																																																																																																							}
+//																																																																																																																																																																																																																																																																										if (swapped == 0){
+//																																																																																																																																																																																																																																																																														break;
+//																																																																																																																																																																																																																																																																																	}
+//																																																																																																																																																																																																																																																																																			}
+//																																																																																																																																																																																																																																																																																					printf("Range: %d\n", data[numOfData-1]-data[0]);
+//																																																																																																																																																																																																																																																																																						}
+//																																																																																																																																																																																																																																																																																						}
+//
