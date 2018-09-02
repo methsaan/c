5829d88b4e2a2f67bb5b2b4995a7c1ac23cc2234
Methsaan <methsaan@gmail.com>
2018-09-02 09:06:58 -0400

    update

--- tests/structTest.c
+++ tests/structTest.c
@@ -10,14 +10,19 @@ struct Square {
 };
 int main(int argc, char* argv){
 	struct Square square;
-	square.x = 8;
-	square.y = 9;
+	scanf("Width (input): %d\n", &square.x);
+	scanf("Height (input): %d", &square.y);
 	square.area = square.x * square.y;
 	square.per = 2*(square.x + square.y);
 	square.hyp = sqrt(square.x * square.x + square.y * square.y);
-	printf("square.x: %d\n", square.x);
-	printf("square.y: %d\n", square.y);
-	printf("square.area: %d\n", square.area);
-	printf("square.per: %d\n", square.per);
-	printf("square.hyp: %f\n", square.hyp);
+	int* X = &square.x;
+	int* Y = &square.y;
+	int* Area = &square.area;
+	int* Per = &square.per;
+	double* Hyp = &square.hyp;
+	printf("square.x: %d        |        \t&square.x (X): %p        \n", square.x, X);
+	printf("square.y: %d        |        \t&square.y (Y): %p        \n", square.y, Y);
+	printf("square.area: %d       |       \t&square.area (Area): %p     \n", square.area, Area);
+	printf("square.per: %d         |        \t&square.per (Per): %p      \n", square.per, Per);
+	printf("square.hyp: %f  |    \t&square.hyp (Hyp): %p        \n", square.hyp, Hyp);
 }
