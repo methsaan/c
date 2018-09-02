78c0d22c9379cda4828c8a0eb8c68c2313041a82
Methsaan <methsaan@gmail.com>
2018-09-02 12:03:06 -0400

    update

--- usefulprograms/dailyschedpdf.c
+++ usefulprograms/dailyschedpdf.c
@@ -5,17 +5,17 @@
 int main(int argv, char *argc){
 	system("clear");
 	printf("___________________________________________________________________________________________________________\n");
-	printf("|________\u54e1\u54e1____________DAILY_SCHEDULE___________wake_up_5:20-6:00_sleep_8:10-9:20___________\u54e1\u54e1________|\n");
+	printf("|________\u00Bf\u00Bf____________DAILY_SCHEDULE___________wake_up_5:20-6:00_sleep_8:10-9:20___________\u00Bf\u00Bf____________|\n");
 	printf("|_________________________________________________________________________________________________________|\n");
 	printf("|  MONDAY  |  TUESDAY   |  WEDNESDAY |  THURSDAY  |  FRIDAY  |  SATURDAY  |  SUNDAY   |  PDs AND BREAKS   |\n");
 	printf("|__________|____________|____________|____________|__________|____________|___________|___________________|\n");
-	printf("|          |            |            |            |          |            |           |     D/B/program   |\n");
-	printf("|    Read  |Smart books |    SQL     |     JS     |  C++/SQL |     D      |  Java     |        PA         |\n");
+	printf("|    Math  |    C++     |  Python    |     D      |Read(sched)|    C      |    D      |     D/B/program   |\n");
+	printf("|    Read  | SB (sched) |    SQL     |     JS     |  C++/SQL |     D      |  Java     |        PA         |\n");
 	printf("|  Program |    Read    |     PA     |C/Smart books| Program |     PP     |    PA     |        PP         |\n");
 	printf("|          |            |            |            |          |            |           |       Maths       |\n");
 	printf("| AFTERNOON| AFTERNOON  | AFTERNOON  | AFTERNOON  | AFTERNOON|  AFTERNOON | AFTERNOON |   AFTERNOON       |\n");
 	printf("|__________|____________|____________|____________|__________|____________|___________|___________________|\n");
-	printf("|     D    |JavaScript/C|Smart books |     PT     |    D     |  Read      |  ft       |  _     PT         |\n");
+	printf("|     D    |JavaScript/C| SB (sched) |     PT     |    D     |Read (sched)|  ft       |  _     PT         |\n");
 	printf("|     PP   |  PT        |    ft      |     D      |    PP    |  C++       |  D        | |      ft         |\n");
 	printf("|Python/Java| PP        |    PP      |     PP     |    C/Java|  PP        |  PP       | |   French study  |\n");
 	printf("|  PT      |  PA        |    PT      | JavaScript |    PT    |  Python    |HTML/JS/C++| |       ft        |\n");
@@ -24,7 +24,7 @@ int main(int argv, char *argc){
 	printf("                                                             |  PP        | PP        | |       PP        |\n");
 	printf("                 Sinhala dictation/Triolingo 5:30pm          |Smart books | D         | |       D         |\n");
 	printf("  B = Buddhism *3       PA = physical activity               |  FT        | FT        | | Python/Java/C++/|\n");
-	printf("                        PT = piano theory                    |  ft        | PP        | |  C/JS/HTML *2   |\n");
+	printf("  SB = smart books      PT = piano theory                    |  ft        | PP        | |  C/JS/HTML *2   |\n");
 	printf("                        PP = practical piano                 |            | C         | |_       FT       |\n");
 	printf("                        D = duolingo                         |            |           |                   |\n");
 	printf("                        FT = free time ft = family time      |____________|___________|___________________|\n");
