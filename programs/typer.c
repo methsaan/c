ac72ed33b0dcc259798b5fddbf027cbaf8758fe5
Methsaan <methsaan@gmail.com>
2018-09-16 10:26:49 -0400

    update;

---
+++ programs/typer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	char ch;
+	puts("Start typing");
+	puts("Press ~ then Enter to stop");
+	for (;;){
+		ch=getchar();
+		if(ch=='~'){
+			break;
+		}
+	}
+	printf("Thanks!\n");
+}
