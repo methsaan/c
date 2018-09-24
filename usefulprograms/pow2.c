86e09a6cbb0a973565f19822149c53daeda3f177
Methsaan <methsaan@gmail.com>
2018-09-23 21:27:58 -0400

    update

--- usefulprograms/pow2.c
+++ usefulprograms/pow2.c
@@ -1,21 +1,31 @@
-#include <math.h>
 #include <stdio.h>
 
-void bla(int *num, int times){
-	int temp = *num;
-	for (int x = 0; x < times; x++){
-		*num += temp;
+int Pow(int num, int raised){
+	int temp = num;
+	if (raised < 0){
+		printf("ERROR: cannot raise to a negative number\n");
+		return '#';
+	}
+	if (num < 0){
+		printf("ERROR: cannot raise negative number\n");
+		return '$';
+	}
+	if (raised == 0){
+		return -1;
+	}else {
+		for (int x = 0; x < raised-1; x++){
+			num *= temp;
+		}
+		return num;
 	}
 }
-int Pow(int x, int y){
-	int temp = x;
-	for (int a = 0; a < y-1; a++){
-		bla(&x, y);
-	}
-	return x;
-}
-
 
 int main(int argc, char *argv){
-	printf("%d\n", Pow(2, 4));
+	int a;
+	int b;
+	printf("Enter first num: ");
+	scanf("%d", &a);
+	printf("Enter second num: ");
+	scanf("%d", &b);
+	printf("%d to the power of %d is %d\n", a, b, Pow(a, b));
 }
