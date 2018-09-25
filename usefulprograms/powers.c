84499369bcf04c3244a75602f7595c9bd91a5a17
Methsaan <methsaan@gmail.com>
2018-09-25 19:38:54 -0400

    update

--- usefulprograms/powers.c
+++ usefulprograms/powers.c
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
-=======
 #include <math.h>
->>>>>>> 104e758f98c7215026ff8634b234977d6ff041c8
 #include <stdio.h>
 
 int Pow(int num, int raised){
 	int temp = num;
-<<<<<<< HEAD
 	// zero and negative numbers; to be continued
 	for (int x = 0; x < raised-1; x++){
 		num *= temp;
 	}
 	return num;
-}
-int main(int argc, char* argv){
-	int a;
-	int b;
-	printf("Enter number: ");
-	scanf("%d", &a);
-	printf("Enter power: ");
-	scanf("%d", &b);
-	printf("%d to the power of %d is %d\n", a, b, Pow(a, b));
-=======
-	int num2 = num;
-	for (int x = 0; x > raised - 1; x++){
-		num2 *= temp;
-	}
-	return num2;
 }
 
 int main(int argc, char *argv){
@@ -37,5 +18,5 @@ int main(int argc, char *argv){
 	printf("Enter second number: ");
 	scanf("%d", &b);
 	printf("%d\n", Pow(a, b));
->>>>>>> 104e758f98c7215026ff8634b234977d6ff041c8
+	return 0;
 }
