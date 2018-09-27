71d0c20535b6aee8de0e3401a5d1d0e423bb2f2e
Methsaan <methsaan@gmail.com>
2018-09-27 07:18:13 -0400

    update

--- usefulprograms/pow.c
+++ usefulprograms/pow.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+long long int Pow(long long int num long long int raised){
+	int temp = num;
+	for (long long int x = 0; x < raised-1; x++){
+		num *= temp;
+	}
+}
 int main(int argc, char* argv){
-	printf("E\n");
+	long long int x;
+	long long int y;
 }
