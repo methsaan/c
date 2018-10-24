b24817a6c8166c72dba796c9bde493dff4ded133
Methsaan <methsaan@gmail.com>
2018-10-24 07:10:57 -0400

    printing factors

--- usefulprograms/primecomp.c
+++ usefulprograms/primecomp.c
@@ -4,10 +4,14 @@ int main(int argc, char *argv){
 	int num;
 	printf("Enter a number: ");
 	scanf("%d", &num);
+	int factors[num];
 	int numOfFactors = 0;
 	for (int x = 0; x < num; x++){
 		if (((double)num)/((double)x) == (double)((int)((double)num/(double)x))){
+			factors[x] = num/x;
 			numOfFactors++;
+		}else {
+			factors[x] = 2147483647;
 		}
 	}
 	if (numOfFactors == 0){
@@ -17,6 +21,10 @@ int main(int argc, char *argv){
 	}else {
 		printf("Your number is composite\n");
 	}
-	printf("Number of factors: %d\n", (numOfFactors*2)-1);
+	printf("Number of factors: %d\n", numOfFactors);
+	for (int x = 0; x < numOfFactors; x++) {
+		printf("%d ", factors[x]);
+	}
+	printf("\n");
 	return 0;
 }
