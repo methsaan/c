a290ebddd19a5b0e0ed827feb247374e0597c0e2
Methsaan <methsaan@gmail.com>
2018-10-24 16:09:53 -0400

    completing factors

--- usefulprograms/primecomp.c
+++ usefulprograms/primecomp.c
@@ -6,22 +6,24 @@ int main(int argc, char *argv){
 	scanf("%d", &num);
 	int factors[num];
 	int numOfFactors = 0;
+	int cnt = 0;
 	for (int x = 0; x < num; x++){
 		if (((double)num)/((double)x) == (double)((int)((double)num/(double)x))){
-			factors[x] = num/x;
+			factors[cnt++] = num/x;
 			numOfFactors++;
-		}else {
-			factors[x] = 2147483647;
 		}
 	}
-	if (numOfFactors == 0){
+	factors[cnt] = 1;
+	numOfFactors++;
+	if (numOfFactors == 1){
 		printf("Your number is neither prime nor composite\n");
-	}else if (numOfFactors == 1){
+	}else if (numOfFactors == 2){
 		printf("Your number is prime\n");
 	}else {
 		printf("Your number is composite\n");
 	}
 	printf("Number of factors: %d\n", numOfFactors);
+	printf("Factors of %d: ", num);
 	for (int x = 0; x < numOfFactors; x++) {
 		printf("%d ", factors[x]);
 	}
