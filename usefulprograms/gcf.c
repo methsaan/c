0d93b672e9070e2d37ce3a29d61797704e52f334
Methsaan <methsaan@gmail.com>
2018-10-25 20:19:23 -0400

    finding the greatest common factor with for loops

--- usefulprograms/gcf.c
+++ usefulprograms/gcf.c
@@ -13,15 +13,44 @@ int main(int argc, char *argv){
 	printf("Enter 2 numbers separated by a comma: ");
 	scanf("%d, %d", &num1, &num2);
 	for (int x = 1; x < num1; x++){
-		if ((double)num1/x == ((double)num1)/((double)x)){
-			factors1[cnt1] = num1/x;
-			cnt1++;
+		if (((double)num1)/((double)x) == (double)((int)((double)num1/(double)x))){
+			factors1[cnt1++] = num1/x;
 			len1++;
 		}
+		if (((double)num2)/((double)x) == (double)((int)((double)num2/(double)x))){
+			factors2[cnt2++] = num2/x;
+			len2++;
+		}
 	}
-	printf("Factors of %d: ", num1);
+	if (factors1[len1-1] != 1){
+		factors1[len1++] = 1;
+	}
+	if (factors2[len2-1] != 1){
+		factors2[len2++] = 1;
+	}
+	//printf("Factors of %d: ", num1);
+	//for (int x = 0; x < len1; x++){
+		//printf("%d ", factors1[x]);
+	//}
+	//printf("\nFactors of %d: ", num2);
+	//for (int x = 0; x < len2; x++){
+		//printf("%d ", factors2[x]);
+	//}
+	//printf("\n");
+	int stat;
 	for (int x = 0; x < len1; x++){
-		printf("%d ", factors1[x]);
+		for (int y = 0; y < len2; y++){
+			if (factors1[x] == factors2[y]){
+				gcf = factors1[x];
+				stat = 1;
+				break;
+			}else {
+				stat = 0;
+			}
+		}
+		if (stat == 1){
+			break;
+		}
 	}
-	printf("\n");
+	printf("GCF: %d\n", gcf);
 }
