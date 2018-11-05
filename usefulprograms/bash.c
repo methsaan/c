28c8fd099799b2ab1feac1cb5173261943f419d7
Methsaan <methsaan@gmail.com>
2018-11-05 06:36:30 -0500

    accepting spaces

--- usefulprograms/bash.c
+++ usefulprograms/bash.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char *argv){
-	wchar_t command[200];
+	char *command;
+	char *command2;
+	char *fullcommand;
 	system("echo \"welcome to bash.c\"");
 	while (1){
+		fullcommand = "";
 		printf("Enter a bash command: ");
-		scanf("%s", command);
-		system(command);
+		scanf("%s%s", command, command2);
+		strcat(fullcommand, command);
+		strcat(fullcommand, " ");
+		strcat(fullcommand, command2);
+		printf("%p\n", fullcommand);
+		system(fullcommand);
 	}
 	return 0;
 }
