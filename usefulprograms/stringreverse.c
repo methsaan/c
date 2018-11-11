2ed2bf1719832d00e98cb681bf36ea0610e32718
Methsaan <methsaan@gmail.com>
2018-11-11 08:03:06 -0500

    printing backward

--- usefulprograms/stringreverse.c
+++ usefulprograms/stringreverse.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv){
 	char string[100];
 	int len = 0;
 	printf("Enter a string: ");
-	fgets(string, 100, stdin);
+	scanf("%s", string);
 	for (int x = 0; x < 100; x++){
 		len++;
 		if ((!isdigit(string[x])) && (!isalpha(string[x])) && (string[x] != 32)){
@@ -13,14 +13,16 @@ int main(int argc, char *argv){
 		}
 	}
 	len--;
-	printf("Your string: %s", string);
-	printf("length: %d\n", len);
+	printf("Your string: %s\n", string);
 	char backward[len];
-	for (int x = 0; x < len; x--){
-		backward[x] = string[len-x];
+	int y = 0;
+	for (int x = len-1; x > -1; x--){
+		backward[y] = string[x];
+		y++;
 	}
+	printf("Backward: ");
 	for (int x = 0; x < sizeof(backward)/sizeof(*backward); x++){
-		printf("%c", backward);
+		printf("%c", backward[x]);
 	}
 	printf("\n");
 }
