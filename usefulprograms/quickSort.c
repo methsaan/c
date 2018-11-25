9838fdcc2490845eec74fa22e703665065c60683
Methsaan <methsaan@gmail.com>
2018-11-25 10:50:37 -0500

    sizeof file 152

--- usefulprograms/quickSort.c
+++ usefulprograms/quickSort.c
@@ -5,24 +5,17 @@
 int main(int argc, char *argv){
 	srand(time(NULL));
 	int len;
+	printf("Enter length: ");
 	scanf("%d", &len);
 	int arr[len];
 	for (int x = 0; x < len; x++){
 		printf("Enter the next element: ");
 		scanf("%d", &arr[x]);
 	}
-	int randomIndex;
-	int temp;
 	for (int x = 0; x < 4; x++){
-		randomIndex = rand()%len;
-		for (int y = 0; y < len; y++){
-			if (arr[y] == arr[randomIndex]){
-				continue;
-			}else if (arr[y] < arr[randomIndex]){
-				temp = arr[y];
-				arr[y] = arr[randomIndex];
-
-			}
-		}
+		stdin
+	}
+	for (int x = 0; x < sizeof(arr)/sizeof(*arr); x++){
+		printf("%d\n", arr[x]);
 	}
 }
