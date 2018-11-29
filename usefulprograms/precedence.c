aff5364155edd6e7906f35021b5614d47986525a
Methsaan <methsaan@gmail.com>
2018-11-29 06:23:15 -0500

    getting numbers and operators

---
+++ usefulprograms/precedence.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+	int numOfOp;
+	printf("Enter number of operations: ");
+	scanf("%d", &numOfOp);
+	char op[numOfOp];
+	int num[numOfOp+1]
+	printf("Enter first number: ");
+	scanf("%d", &num[0]);
+	for (int x = 0; x < numOfOp; x++){
+		printf("Enter next operator: ");
+		scanf("%c", &op[x]);
+		printf("Enter next number")
+	}
+	scanf("%d", &num[sizeof(num)/sizeof(*num)-1]);
+}
