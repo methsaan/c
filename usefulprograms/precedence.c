5529dd0e7942bf0af88bfa9778771eaeff086c61
Methsaan <methsaan@gmail.com>
2018-11-29 21:20:23 -0500

    input equation

--- usefulprograms/precedence.c
+++ usefulprograms/precedence.c
@@ -5,13 +5,35 @@ int main(int argc, char *argv) {
 	printf("Enter number of operations: ");
 	scanf("%d", &numOfOp);
 	char op[numOfOp];
-	int num[numOfOp+1]
+	int num[numOfOp+1];
+	int cnt1 = 0;
+	int cnt2 = 0;
 	printf("Enter first number: ");
-	scanf("%d", &num[0]);
+	scanf("%d", &num[cnt1]);
+	cnt1++;
 	for (int x = 0; x < numOfOp; x++){
+		getchar();
 		printf("Enter next operator: ");
-		scanf("%c", &op[x]);
-		printf("Enter next number")
+		op[cnt2] = getchar();
+		getchar();
+		if (x < numOfOp-1){
+			printf("Enter next number: ");
+			scanf("%d", &num[cnt1]);
+			cnt1++;
+		}
+		cnt2++;
 	}
-	scanf("%d", &num[sizeof(num)/sizeof(*num)-1]);
+	printf("Enter last number: ");
+	scanf("%d", &num[cnt1]);
+	cnt1 = 0;
+	cnt2 = 0;
+	printf("%d ", num[cnt1]);
+	cnt1++;
+	for (int x = 0; x < sizeof(num)/sizeof(*num) + sizeof(op)/sizeof(*op)-2; x++){
+		printf("%c ", op[cnt2]);
+		printf("%d ", num[cnt1]);
+		cnt2++;
+		cnt1++;
+	}
+	printf("%d\n", num[cnt1-1]);
 }
