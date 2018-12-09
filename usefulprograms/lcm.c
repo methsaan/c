9a7c63cbf94bcaf26b49fbf731bf52fcd0b2b9ed
Methsaan <methsaan@gmail.com>
2018-12-09 14:17:58 -0500

    dealing with all possibilities

--- usefulprograms/lcm.c
+++ usefulprograms/lcm.c
@@ -8,24 +8,24 @@ int main(int argc, char *argv){
 	printf("Enter two numbers: ");
 	scanf("%d%s%d", &num1, arr, &num2);
 	int lcm;
-	int multiples1[45];
-	int multiples2[45];
-	for (int x = 1; x < 46; x++){
+	int multiples1[55];
+	int multiples2[55];
+	for (int x = 1; x < 56; x++){
 		multiples1[x-1] = num1*x;
 		multiples2[x-1] = num2*x;
 	}
 	printf("Multiples of %d: ", num1);
-	for (int x = 0; x < sizeof(multiples1)/sizeof(*multiples1); x++){
+	for (int x = 0; x < 55; x++){
 		printf("%d ", multiples1[x]);
 	}
 	printf("\nMultiples of %d: ", num2);
-	for (int x = 0; x < sizeof(multiples2)/sizeof(*multiples2); x++){
+	for (int x = 0; x < 55; x++){
 		printf("%d ", multiples2[x]);
 	}
 	printf("\n");
 	int status;
-	for (int x = 0; x < 45; x++){
-		for (int y = 0; y < 45; y++){
+	for (int x = 0; x < 55; x++){
+		for (int y = 0; y < 55; y++){
 			if (multiples1[x] == multiples2[y]){
 				lcm = multiples1[x];
 				status = 1;
