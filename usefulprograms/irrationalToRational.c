6d5235d66558fbd5a6df5753dfa9b43321484cee
Methsaan <methsaan@gmail.com>
2018-12-13 06:21:44 -0500

    find approximation for an irrational number

---
+++ usefulprograms/irrationalToRational.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int closest(int num, int arr[]){
+}
+int main(int argc, char *argv){
+	double irrational;
+	printf("Enter an irrational number: ");
+	scanf("%lf", &irrational);
+	printf("%f\n", irrational);
+}
