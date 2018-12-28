b7ceacd5ba54914f8b63a2d0d29d410d1322a43a
Methsaan Wick@kali <methsaan@gmail.com>
2018-12-28 12:31:08 -0500

    pointer/file

--- tests/file_handle_1.c
+++ tests/file_handle_1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char argv[]) {
-
+    int a = 100;
+    char c = 'c';
+    FILE* fp = fopen("/tmp/file1.txt","w");
+    fprintf(fp, "This is file content without formatting");
+    fprintf(fp, "This is file content with formatting\n");
+    fprintf(fp, "This is file content and the character is %c and number %d\n",c,a);
 }
-
