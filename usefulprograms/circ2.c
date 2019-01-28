d33250ec592bded6d3c206296fe41abcbfa6dbe6
Methsaan Wick@kali <methsaan@gmail.com>
2019-01-28 16:45:51 -0500

    changing schedule information

--- usefulprograms/circ2.c
+++ usefulprograms/circ2.c
@@ -8,12 +8,13 @@ int main(int argc, char *argv){
  	const double PI = 3.14159265358972;
  	double height;
 	double radius;
- 	printf("Options: circle_area, circle_circ, cylinder_area, cylinder_volume, cone_area, cone_vol, sphere_area, sphere_vol\nEnter your option: ");
+ 	printf("Options: circle_circ, circle_area, cylinder_area, cylinder_volume, cone_area, cone_vol, sphere_area, sphere_vol\nEnter your option: ");
 	scanf("%s", option);
 	if (strcmp(option, "circle_circ") == 0) {
 		printf("Enter circumference: ");
 		scanf("%lf", &circumference);
 		radius = circumference/PI/2.0;
-		printf("Radius: %f\n", radius);
+		printf("Radius: %.20g\n", radius);
+	}else if (strcmp(option, "circle_area") == 0) {
 	}
 }
