3b348c587ee7796131538d862812249d88e2915e
Methsaan <methsaan@gmail.com>
2019-02-02 19:36:57 -0500

    changing schedule information

--- usefulprograms/radiuscalc.c
+++ usefulprograms/radiuscalc.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv){
  	const double PI = 3.14159265358972;
  	double height;
 	double radius;
- 	printf("Options: circle_circ, circle_area, cylinder_volume, cylinder_area, cone_area, cone_vol, sphere_area, sphere_vol\nEnter your option: ");
+ 	printf("Options: circle_circ, circle_area, cylinder_volume, cylinder_area\nEnter your option: ");
 	scanf("%s", option);
 	if (strcmp(option, "circle_circ") == 0) {
 		printf("Enter circumference: ");
@@ -24,26 +24,18 @@ int main(int argc, char *argv){
 		printf("radius: %.20g\n", radius);
 	}else if (strcmp(option, "cylinder_area")) {
 		printf("Enter volume: ");
+		scanf("%lf", &area);
+		printf("Enter height: ");
+		scanf("%lf", &height);
+		radius = (sqrt(area/PI/2/2/PI/height * 4) / 2) - 0.45;
+		printf("Radius: %.20g\n", radius);
+	}else if (strcmp(option, "cylinder_volume")) {
+		printf("Enter surface area: ");
 		scanf("%lf", &volume);
 		printf("Enter height: ");
 		scanf("%lf", &height);
 		area = volume/height;
 		radius = sqrt(area/PI);
 		printf("radius: %.20g\n", radius);
-	}else if (strcmp(option, "cylinder_volume")) {
-		printf("Enter surface area: ");
-		scanf("%lf", &area);
-		printf("Enter height: ");
-		scanf("%lf", &height);
-		//r = 1
-		//r*r+r = 2
-		//r = 2
-		//r*r+r = 6
-		//r = 3
-		//r*r+r = 12
-		//r = 4
-		//r*r+r = 20
-		radius = area/PI/2/2/PI/height;
-		printf("Radius: %.20g\n", radius);
 	}
 }
