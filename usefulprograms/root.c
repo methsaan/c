32b7932ed32f3070e75af7ad19e8352afb492b40
Methsaan <methsaan@gmail.com>
2019-02-07 06:32:52 -0500

    pow function

---
+++ usefulprograms/root.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv) {
+	double num;
+	double root;
+	printf("Enter a number: ");
+	scanf("%lf", &num);
+	printf("Enter a root: ");
+	scanf("%lf", &root);
+	printf("%f\n", (double)pow(num, 1/root));
+}
