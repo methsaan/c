c246bd807a8ecabbcc9decc96fd414c0c784d38a
Methsaan <methsaan@gmail.com>
2019-02-10 20:27:29 -0500

    finding the height of a parrallelogram

---
+++ usefulprograms/parrallelogram.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char *argv) {
+	double base;
+	double adjacentSide;
+	double height;
+	double area;
+	printf("Enter base of parrallelogram: ");
+	scanf("%lf", &base);
+	printf("Enter adjacent side of parrallelogram: ");
+	scanf("%lf", &adjacentSide);
+	height = sqrt(sqrt((adjacentSide)-pow(base, 2)));
+	printf("%lf\n", height);
+}
