b9c383d9fa11fed17662e7bae57cd74bf0f28525
Methsaan <methsaan@gmail.com>
2019-02-14 06:22:29 -0500

    factors of powers of six

--- usefulprograms/factors.c
+++ usefulprograms/factors.c
@@ -2,12 +2,12 @@
 #include<math.h>
 
 int main(int argc, char *argv){
-	for (int i = 1; i < pow(2, 63)-1; i++) {
+	for (int i = 1; i < pow(2, 63)-1; i*=6) {
 		int num = i;
 		int factors[num];
 		int numOfFactors = 0;
 		int cnt = 0;
-		for (int x = 0; x < num; x++){
+		for (long long int x = 0; x < num; x++){
 			if (((double)num)/((double)x) == (double)((int)((double)num/(double)x))){
 				factors[cnt++] = num/x;
 				numOfFactors++;
