664fbbd6a81916b6314af82e78008980e57a40c4
Methsaan <methsaan@gmail.com>
2019-02-22 19:32:34 -0500

    adding the repeated part to x_X_100

--- usefulprograms/decToFrac.c
+++ usefulprograms/decToFrac.c
@@ -2,22 +2,19 @@
 #include <math.h>
 
 int main(int argc, char *argv) {
-	long double x;
-	long double repeating;
+	double x;
+	double repeating;
 	char repeatingstr[5];
 	printf("Enter a number with repeating digits: ");
 	scanf("%lf", &x);
 	printf("Enter the part of the number that repeats: ");
 	scanf("%lf", &repeating);
 	sprintf(repeatingstr, "%d", repeating);
-	long double x_X_100 = x*100;
-	for (int i = 4; i < 10; i++) {
-		x_X_100 += repeating* (pow(10, -(i*sizeof(repeatingstr))));
+	double x_X_100 = (int)(x*100);
+	for (int i = sizeof(repeatingstr); i < 7; i += sizeof(repeatingstr)) {
+		x_X_100 += repeating * (pow(10, -(i+1)));
 	}
-	printf("%lf", x_X_100);
-	printf(" - %lf", x);
-	printf(" = %lf\n", x_X_100-x);
-	//printf("%lf\n", x_X_100-x);
-	//printf("____________   =   %lf \n\n", (x_X_100-x)/99);
-	//printf("%lf\n", 99);
+	printf("%f", x_X_100);
+	printf(" - %f", x);
+	printf(" = %f\n", x_X_100-x);
 }
