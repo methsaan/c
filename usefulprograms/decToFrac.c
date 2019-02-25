badbcfed63381d64d527cc4c77897e668462a716
Methsaan <methsaan@gmail.com>
2019-02-25 07:19:05 -0500

    converting the repeating number

--- usefulprograms/decToFrac.c
+++ usefulprograms/decToFrac.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-//
+#include <stdlib.h>
+
+int numOfDigits(int num) {
+	int count = 0;
+	while (num > 0) {
+		num = num / 10;
+		count++;
+	}
+	return count;
+}
 int main(int argc, char *argv) {
 	double x;
-	int repeating;
-	int repeatingLength = 0;
-	int temp = repeating;
-	printf("Enter a number with repeating digits: ");
+	printf("Enter a number with repeated digits: ");
 	scanf("%lf", &x);
-	printf("Enter the part of the number that repeats: ");
-	scanf("%lf", &repeating);
-	while(temp != 0) {
-	    temp /= 10;
-	    repeatingLength++;
+	int repeating;
+	printf("Enter the repeating part of the number: ");
+	scanf("%d", &repeating);
+	int repeatingLength = numOfDigits(repeating);
+	double x_X_100 = x * 100;
+	for (int x = 4+repeatingLength; x < 7+(repeatingLength*3); x += repeatingLength) {
+		x_X_100 += repeating * pow(10, -x);
 	}
-	printf("%d\n\n\n", repeatingLength);
-	double x_X_10 = (x*10);
-	for (int a = 4; a < 7; a++) {
-		x_X_10 += (double)repeating/pow(10, -(4*a+repeatingLength));
-	}
-	printf("%f", x_X_10);
-	printf(" - %f", x);
-	printf(" = %f\n", x_X_10-x);
+	printf("  __\n");
+	printf("%d.%d  =  %d / %d\n", (int)x, repeating, (int)round(x_X_100-x), 99);
 }
