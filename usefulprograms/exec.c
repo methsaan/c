0eebc9e248013a69ec8d7add5546787b7810b593
Methsaan <methsaan@gmail.com>
2019-03-08 06:54:35 -0500

    changing schedule information

--- usefulprograms/exec.c
+++ usefulprograms/exec.c
@@ -7,11 +7,13 @@ int main(int argc, char *argv) {
 	FILE *fp2;
 	char code[500];
 	char origCode[500];
+	char newCode[700];
 	fp2 = fopen("execfile.c" , "r");
 	int c,nl=0,nc=0;
 	c=getc(fp2);
 	origCode[0] = c;
 	int idx = 1;
+	int lines = 0;
 	while(c!=EOF){
 		if(c=='\n')
 		nl++;
@@ -19,11 +21,21 @@ int main(int argc, char *argv) {
 		c=getc(fp2);
 		origCode[idx] = c;
 		idx++;
+		if (c == '\n') {
+			lines++;
+		}
+		
 	}
 	fp = fopen("execfile.c", "w+");
+	int idx2 = 0;
+	for (int x = 0; origCode[x] != '}'; x++) {
+		newCode[x] = origCode[x];
+	}
 	while (strcmp(code, "return 0;\n") != 0) {
 		printf("Enter code: ");
 		fgets(code, 500, stdin);
-		
+		for (int x = 0; x < sizeof(code)/sizeof(*code); x++) {
+			newCode
+		}
 	}
 }
