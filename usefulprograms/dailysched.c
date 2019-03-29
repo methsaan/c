24df747ebcf2be4d2a8af8c4d459fb6ce2e609cf
Methsaan <methsaan@gmail.com>
2019-03-29 18:29:07 -0400

    changing schedule information

--- usefulprograms/dailysched.c
+++ usefulprograms/dailysched.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv){
 	fscanf(fp, "%s", three);
 	fscanf(fp, "%s", four);
 	int dayy = atoi(one);
-	dayy = 0;
+	dayy = 1;
 	int randnum = atoi(two);
 	int randday = atoi(three);
 	int randnum2 = atoi(four);
