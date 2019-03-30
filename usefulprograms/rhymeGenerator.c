d01f7bdd7e95e3c656425ec9f648cebe2d5e37dc
Methsaan <methsaan@gmail.com>
2019-03-30 12:51:15 -0400

    new schedule

---
+++ usefulprograms/rhymeGenerator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv) {
+	char word[100];
+	char consonants[21]
+	printf("Enter a word: ");
+	scanf("%s", word);
+	char *words[26] = {"a", "i", "ab", "am", "an", "as", "at", "be", "by", "do", "go", "hi", "if", "in", "is", "it", "me", "my", "no", "of", "ok", "to", "tv", "up", "us", "we"};
+	for (int x = 0; x < 26; x++) {
+			
+	}
+}
