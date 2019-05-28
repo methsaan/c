09c6cdba5f87aeaa11050690fa5f38a16df624d7
Methsaan <methsaan@gmail.com>
2019-05-28 06:39:43 -0400

    calculate exact age

--- usefulprograms/agecalc.c
+++ usefulprograms/agecalc.c
@@ -3,7 +3,7 @@
 
 struct date {
 	int year;
-	char *month = "";
+	int month;
 	int date;
 };
 
@@ -11,27 +11,47 @@ int main(int argc, char *argv) {
 	struct date birth;
 	struct date current;
 	double age;
-	char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-	printf("When were you born (y-m-d): ");
-	scanf("%d-%s-%d", &birth.year, birth.month, &birth.date);
-	printf("What is the current date (y-m-d): ");
-	scanf("%d-%s-%d", &current.year, current.month, &current.date);
-	int currentmon;
-	int birthmon;
-	for (int x = 0; x < 12; x++) {
-		if (strcmp(months[x], birth.month) == 0) {
-			birthmon = x;
-		}
-	}
-	for (int x = 0; x < 12; x++) {
-		if (strcmp(months[x], current.month) == 0) {
-			currentmon = x;
-		}
-	}
+	printf("When were you born (yyyy-mm-dd): ");
+	scanf("%d-%d-%d", &birth.year, &birth.month, &birth.date);
+	printf("What is the current date (yyyy-mm-dd): ");
+	scanf("%d-%d-%d", &current.year, &current.month, &current.date);
 	printf("%d\n", current.year);
-	printf("%s\n", currentmon);
+	printf("%d\n", current.month);
 	printf("%d\n", current.date);
 	printf("%d\n", birth.year);
-	printf("%s\n", birthmon);
+	printf("%d\n", birth.month);
 	printf("%d\n", birth.date);
+	int days;
+	int dayCarryover = 0;
+	int months;
+	int monthCarryover = 0;
+	int years;
+	int yearCarryover = 0;
+	if (birth.date < current.date) {
+		days = current.date - birth.date;
+	}else {
+		days = (current.date+30) - birth.date;
+		dayCarryover = 1;
+	}
+	if (birth.month < current.month) {
+		if (dayCarryover) {
+			months = (current.month-1) - birth.month;
+		}else {
+			months = current.month - birth.month;
+		}
+	}else {
+		if (dayCarryover) {
+			months = ((current.month-1)+12) - birth.month;
+		}else {
+			months = (current.month+12) - birth.month;
+		}
+		monthCarryover = 1;
+	}
+	if (monthCarryover) {
+		years = (current.year-1) - birth.year;
+	}else {
+		years = current.year - birth.year;
+	}
+	age = years + ((1.0/12.0)*months) + ((1.0/365.0)*days);
+	printf("%d years, %d months, %d days\nage: %.10f\n", years, months, days, age);
 }
