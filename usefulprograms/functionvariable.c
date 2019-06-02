2ae4631708dd5966cd4993724425587476eb8af5
Methsaan <methsaan@gmail.com>
2019-06-02 08:50:52 -0400

    changing schedule information

--- usefulprograms/functionvariable.c
+++ usefulprograms/functionvariable.c
@@ -20,16 +20,10 @@ int main(int argc, char *argv) {
 		int y;
 		coords[x+5].x = x;
 		coords[x+5].y = y;
-		convertToComputer(&coords[x+5].x, &coords[x+5].y);	}
+		convertToComputer(&coords[x+5].x, &coords[x+5].y);
+	}
 	for (int i = 0; i < 10; i++) {
-		system("clear");
-		for (int j = 0; j < coords[i].y-1; j++) {
-			printf("\n");
-		}
-		for (int j = 0; j < coords[i].x-1; j++) {
-			printf(" ");
-		}
-		printf("x\n");
+		printf("(1, 0)\n", coords[i].x, coords[i].y);
 		for (long int j = 0; j < 30000000; j++) {
 			;
 		}
