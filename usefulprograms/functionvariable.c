72148f81ec2bd32113eeab099bb54d6c6059f29a
Methsaan <methsaan@gmail.com>
2019-06-04 20:57:56 -0400

    creating function value

--- usefulprograms/functionvariable.c
+++ usefulprograms/functionvariable.c
@@ -18,12 +18,9 @@ int main(int argc, char *argv) {
 	struct coord coords[10];
 	for (int x = -5; x < 5; x++) {
 		int y;
-		coords[x+5].x = x;
-		coords[x+5].y = y;
-		convertToComputer(&coords[x+5].x, &coords[x+5].y);
+		printf("%d %d\n", x, y);
 	}
 	for (int i = 0; i < 10; i++) {
-		printf("(1, 0)\n", coords[i].x, coords[i].y);
 		for (long int j = 0; j < 30000000; j++) {
 			;
 		}
