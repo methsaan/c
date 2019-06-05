e7ab96c3fa27a3a27c756e6efda230510bdc41e6
Methsaan <methsaan@gmail.com>
2019-06-04 20:57:56 -0400

    creating function value

--- usefulprograms/linearfunction.c
+++ usefulprograms/linearfunction.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char *argv) {
 	char function[50];
 	printf("Enter function to graph: ");
 	scanf("%s", function);
+	printf("%s\n", function);
+	int functionlen;
+	for (functionlen = 0; functionlen < 50; functionlen++) {
+		if (!isalpha(function[functionlen])) {
+			break;
+		}
+	}
+	for (int x = 0; x < functionlen; x++) {
+		printf("%c", function[x]);
+	}
+	printf("\n");
 	FILE *fp = fopen("functionvariable.c", "w");
 	fprintf(fp, "#include <stdio.h>\n");
 	fprintf(fp, "#include <stdlib.h>\n");
@@ -22,13 +34,18 @@ int main(int argc, char *argv) {
 	fprintf(fp, "int main(int argc, char *argv) {\n");
 	fprintf(fp, "\tstruct coord coords[10];\n");
 	fprintf(fp, "\tfor (int x = -5; x < 5; x++) {\n");
-	fprintf(fp, "\t\tint %s;\n", function);
-	fprintf(fp, "\t\tcoords[x+5].x = x;\n");
-	fprintf(fp, "\t\tcoords[x+5].y = y;\n");
-	fprintf(fp, "\t\tconvertToComputer(&coords[x+5].x, &coords[x+5].y);\n");
+	fprintf(fp, "\t\tint ");
+	for (int x = 0; x < functionlen; x++) {
+		fprintf(fp, "%c", function[x]);
+	}
+	fprintf(fp, ";\n");
+	fprintf(fp, "\t\tprintf(\"%%d %%d\\n\", x, y);\n", function);
+	//fprintf(fp, "\t\tcoords[x+5].x = x;\n");
+	//fprintf(fp, "\t\tcoords[x+5].y = y;\n");
+	//fprintf(fp, "\t\tconvertToComputer(&coords[x+5].x, &coords[x+5].y);\n");
 	fprintf(fp, "\t}\n");
 	fprintf(fp, "\tfor (int i = 0; i < 10; i++) {\n");
-	fprintf(fp, "\t\tprintf(\"(%d, %d)\\n\", coords[i].x, coords[i].y);\n");
+	//fprintf(fp, "\t\tprintf(\"(%d, %d)\\n\", coords[i].x, coords[i].y);\n");
 	//fprintf(fp, "\t\tsystem(\"clear\");\n");
 	//fprintf(fp, "\t\tfor (int j = 0; j < coords[i].y-1; j++) {\n");
 	//fprintf(fp, "\t\t\tprintf(\"\\n\");\n");
