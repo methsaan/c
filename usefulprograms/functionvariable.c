f7b6b10cc02db01d24b3b764df137f74a68a3542
Methsaan <methsaan@gmail.com>
2019-06-06 19:40:30 -0400

    moving linear function

--- usefulprograms/functionvariable.c
+++ usefulprograms/functionvariable.c
@@ -8,20 +8,31 @@ struct coord {
 };
 
 void convertToComputer(int *x, int *y) {
-	*x += 10;
+	*x += 20;
 	*x *= 2;
 	*y = -(*y);
-	*y += 10;
+	*y += 20;
 }
 
 int main(int argc, char *argv) {
-	struct coord coords[10];
-	for (int x = -5; x < 5; x++) {
-		int y;
-		printf("%d %d\n", x, y);
+	struct coord coords[20];
+	for (int x = -10; x < 10; x++) {
+		int y = -x*1/3;
+		coords[x+10].x = x;
+		coords[x+10].y = y;
+		convertToComputer(&coords[x+10].x, &coords[x+10].y);
 	}
-	for (int i = 0; i < 10; i++) {
-		for (long int j = 0; j < 30000000; j++) {
+	for (int i = 0; i < 20; i++) {
+		printf("(1, 0)\n", coords[i].x, coords[i].y);
+		system("clear");
+		for (int j = 0; j < coords[i].y-1; j++) {
+			printf("\n");
+		}
+		for (int j = 0; j < coords[i].x-1; j++) {
+			printf(" ");
+		}
+		printf("x\n");
+		for (long int j = 0; j < 15000000; j++) {
 			;
 		}
 	}
