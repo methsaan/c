67f2ed330eaaf7384bccd1b6f06022485440c653
Methsaan <methsaan@gmail.com>
2019-06-06 20:46:54 -0400

    more plots

--- usefulprograms/functionvariable.c
+++ usefulprograms/functionvariable.c
@@ -8,21 +8,21 @@ struct coord {
 };
 
 void convertToComputer(int *x, int *y) {
-	*x += 20;
+	*x += 30;
 	*x *= 2;
 	*y = -(*y);
-	*y += 20;
+	*y += 30;
 }
 
 int main(int argc, char *argv) {
-	struct coord coords[20];
-	for (int x = -10; x < 10; x++) {
-		int y = -x*1/3;
-		coords[x+10].x = x;
-		coords[x+10].y = y;
-		convertToComputer(&coords[x+10].x, &coords[x+10].y);
+	struct coord coords[30];
+	for (int x = -15; x < 15; x++) {
+		int y = x;
+		coords[x+15].x = x;
+		coords[x+15].y = y;
+		convertToComputer(&coords[x+15].x, &coords[x+15].y);
 	}
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 30; i++) {
 		printf("(1, 0)\n", coords[i].x, coords[i].y);
 		system("clear");
 		for (int j = 0; j < coords[i].y-1; j++) {
@@ -31,8 +31,8 @@ int main(int argc, char *argv) {
 		for (int j = 0; j < coords[i].x-1; j++) {
 			printf(" ");
 		}
-		printf("x\n");
-		for (long int j = 0; j < 15000000; j++) {
+		printf("(%d, %d)\n", coords[i].x/2-30, -(coords[i].y-30));
+		for (long int j = 0; j < 6000000; j++) {
 			;
 		}
 	}
