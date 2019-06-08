abf5706693e0f5c537a95f006be97acb1f70700d
Methsaan <methsaan@gmail.com>
2019-06-08 09:51:49 -0400

    print coordinate

--- usefulprograms/functionvariable.c
+++ usefulprograms/functionvariable.c
@@ -17,22 +17,22 @@ void convertToComputer(int *x, int *y) {
 int main(int argc, char *argv) {
 	struct coord coords[30];
 	for (int x = -15; x < 15; x++) {
-		int y = abs(x+10);
+		int y = x+(x*x/10);
 		coords[x+15].x = x;
 		coords[x+15].y = y;
 		convertToComputer(&coords[x+15].x, &coords[x+15].y);
 	}
 	for (int i = 0; i < 30; i++) {
-		printf("(1, 0)\n", coords[i].x, coords[i].y);
 		system("clear");
-		for (int j = 0; j < coords[i].y-1; j++) {
+		printf("(%d, %d)\n", coords[i].x/2-30, -(coords[i].y-30));
+		for (int j = 0; j < coords[i].y-2; j++) {
 			printf("\n");
 		}
 		for (int j = 0; j < coords[i].x-1; j++) {
 			printf(" ");
 		}
-		printf("(%d, %d)\n", coords[i].x/2-30, -(coords[i].y-30));
-		for (long int j = 0; j < 16000000; j++) {
+		printf("X\n");
+		for (long int j = 0; j < 300000; j++) {
 			;
 		}
 	}
