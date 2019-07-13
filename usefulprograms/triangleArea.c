56d64fa7d759fe0e815a5ad1c2cf2b46c8c84d79
Methsaan <methsaan@gmail.com>
2019-07-13 11:43:39 -0400

    triangle type

--- usefulprograms/triangleArea.c
+++ usefulprograms/triangleArea.c
@@ -18,7 +18,20 @@ int main(int argc, char *argv[]) {
 	side2 = sqrt(pow(abs(x3-x1), 2)+pow(abs(y3-y1), 2));
 	side3 = sqrt(pow(abs(x3-x2), 2)+pow(abs(y3-y2), 2));
 	semiperimeter = (side1+side2+side3)/2;
+	if (side1 == side2 && side2 == side2 == side3) {
+		printf("Equilateral triangle\n");
+	}else if (side1 == side2 && side2 != side3) {
+		if (pow(side3, 2)/2 == side2) {
+			printf("Isosceles right triangle\n");
+		}else if (pow(side3, 2)/2 < side2) {
+			printf("Isosceles acute triangle\n");
+		}else {
+			printf("Isosceles obtuse triangle\n");
+		}
+	}else {
+		printf("Scalene triangle\n");
+	}
 	area = sqrt(semiperimeter*(semiperimeter-side1)*(semiperimeter-side2)*(semiperimeter-side3));
-	printf("%g\n", area);
+	printf("Area: %g\n", area);
 
 }
