add9589f878e84db7b1d976373d6b2a64c7263f7
Methsaan <methsaan@gmail.com>
2019-07-13 11:05:08 -0400

    area of a triangle by coordinates

--- usefulprograms/triangleArea.c
+++ usefulprograms/triangleArea.c
@@ -10,12 +10,15 @@ int main(int argc, char *argv[]) {
 	double x3 = atof(argv[5]);
 	double y3 = atof(argv[6]);
 	double side1;
-	double height;
 	double side2;
 	double side3;
-	double d;
+	double semiperimeter; 
+	double area;
 	side1 = sqrt(pow(abs(x2-x1), 2)+pow(abs(y2-y1), 2));
 	side2 = sqrt(pow(abs(x3-x1), 2)+pow(abs(y3-y1), 2));
 	side3 = sqrt(pow(abs(x3-x2), 2)+pow(abs(y3-y2), 2));
-	printf("%f, %f\n", y1, x2);
+	semiperimeter = (side1+side2+side3)/2;
+	area = sqrt(semiperimeter*(semiperimeter-side1)*(semiperimeter-side2)*(semiperimeter-side3));
+	printf("%g\n", area);
+
 }
