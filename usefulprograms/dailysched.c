bd00fcbcb17aea643beefeffd24f14dcc44dff8e
Methsaan <methsaan@gmail.com>
2019-09-05 21:38:29 -0400

    day tracker

--- usefulprograms/dailysched.c
+++ usefulprograms/dailysched.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
 	char *days_of_week[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 	char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 	char *timea = __TIME__;
-	if ((timea[0] == '2') && (timea[1] == '1' && timea[3] == '3')) {
+	if ((timea[0] == '0') && (timea[1] == '5' && timea[3] == '1')) {
 		FILE *fpw = fopen("schedfile", "w");
 		fprintf(fpw, "%d\n", dayx);
 		fprintf(fpw, "%d\n", rand()%2);
