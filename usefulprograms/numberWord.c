d9203139e0f1c922a28ec22e16a5f6bd6f379356
Methsaan <methsaan@gmail.com>
2020-01-21 05:42:46 -0500

    day tracker

--- usefulprograms/numberWord.c
+++ usefulprograms/numberWord.c
@@ -7,13 +7,13 @@ char *intToStr(int num) {
 	int numvar = num;
 	char *ones[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen" "nineteen"};
 	char *placevalues[] = {"", "ty", "hundred", "thousand", "ty thousand", "hundred thousand", "million"};
-	char numchar[8];
+	char *numchar;
 	for (int x = 0; x < 8; x++) {
-		numchar[x] = num%((int)(pow(10, 8-x)));
+		strcat(numchar, num%((int)(pow(10, 8-x))));
 	}
 	return numchar;
 }
 
 int main(int argc, char *argv[]) {
-	printf("%s\n", intToStr(2312));
+	printf("%s\n", intToStr(argv[0]));
 }
