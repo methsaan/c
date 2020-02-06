9e69f65fc5a150ff34f6a5417455587b44a83179
Methsaan <methsaan@gmail.com>
2020-02-06 07:40:16 -0500

    place value and order of letter positions

--- usefulprograms/romanNumeral.c
+++ usefulprograms/romanNumeral.c
@@ -3,29 +3,26 @@
 
 int main(int argc, char *argv[]) {
 	char digits[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-	char num[4];
+	int digDec[] = {1000, 500, 100, 50, 10, 5, 1};
+	int ranges[] = {1, 10, 100, 1000};
+	int num;
 	printf("Enter number from 1-1000: ");
-	scanf("%s", num);
-	int decDigits[4] = {num[3]-'0', num[2]-'0', num[1]-'0', num[0]-'0'};
-	char romanNumeral[36] = {0x0};
+	scanf("%d", &num);
+	char romNumeral[20] = "";
+	int pVal[][] = {{0, -1, -1, -1}, {0, 0, -1, -1}, {0, 0, 0, -1}, {0, 1, -1, -1}, {1, -1, -1, -1}, {1, 0, -1, -1}, {1, 0, 0, -1}, {1, 0, 0, 0}, {0, 2, -1, -1}, {2, -1, -1, -1}};
+	int tempNum = num;
 	int cnt = 0;
-	for (int cnt2 = 0; cnt2 < decDigits[3]; cnt2++) {
-		romanNumeral[cnt++] = 'M';
+	while (1) {
+		if (tempNum >= 1000 && tempNum < 10000) {
+			int idx = tempNum / 1000;
+			int pValTemp[4];
+			for (int x = 0; x < 4; x++) {
+				pValTemp[x] = pVal[idx][x];
+			}
+			for (int x = 0; x < 4; x++) {
+				romNumeral[cnt] = digits[6]+pValTemp[x]
+				cnt++;
+			}
+		}
 	}
-	printf("%d\n", cnt);
-	for (int cnt2 = 0; cnt2 < decDigits[2]; cnt2++) {
-		romanNumeral[cnt++] = 'C';
-	}
-	printf("%d\n", cnt);
-	for (int cnt2 = 0; cnt2 < decDigits[1]; cnt2++) {
-		romanNumeral[cnt++] = 'X';
-	}
-	printf("%d\n", cnt);
-	for (int cnt2 = 0; cnt2 < decDigits[0]; cnt2++) {
-		romanNumeral[cnt++] = 'I';
-	}
-	printf("%d\n", cnt);
-	printf("%s\n", romanNumeral);
-	//3835
-	//MMMDCCCXXXV
 }
