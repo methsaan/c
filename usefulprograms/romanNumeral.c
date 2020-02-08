edc16ce2f488457acbd8994cbb99dd7bcaec1cc5
Methsaan <methsaan@gmail.com>
2020-02-08 13:28:55 -0500

    day tracker

--- usefulprograms/romanNumeral.c
+++ usefulprograms/romanNumeral.c
@@ -3,13 +3,11 @@
 
 int main(int argc, char *argv[]) {
 	char digits[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-	int digDec[] = {1000, 500, 100, 50, 10, 5, 1};
-	int ranges[] = {1, 10, 100, 1000};
 	int num;
-	printf("Enter number from 1-1000: ");
+	printf("Enter number from 1-10000: ");
 	scanf("%d", &num);
 	char romNumeral[20] = "";
-	int pVal[][] = {{0, -1, -1, -1}, {0, 0, -1, -1}, {0, 0, 0, -1}, {0, 1, -1, -1}, {1, -1, -1, -1}, {1, 0, -1, -1}, {1, 0, 0, -1}, {1, 0, 0, 0}, {0, 2, -1, -1}, {2, -1, -1, -1}};
+	int pVal[10][4] = {{0, -1, -1, -1}, {0, 0, -1, -1}, {0, 0, 0, -1}, {0, 1, -1, -1}, {1, -1, -1, -1}, {1, 0, -1, -1}, {1, 0, 0, -1}, {1, 0, 0, 0}, {0, 2, -1, -1}, {2, -1, -1, -1}};
 	int tempNum = num;
 	int cnt = 0;
 	while (1) {
@@ -20,9 +18,14 @@ int main(int argc, char *argv[]) {
 				pValTemp[x] = pVal[idx][x];
 			}
 			for (int x = 0; x < 4; x++) {
-				romNumeral[cnt] = digits[6]+pValTemp[x]
+				romNumeral[cnt] = pValTemp[x] == -1 ? '\0' : digits[6+pValTemp[x]];
 				cnt++;
 			}
 		}
 	}
+	printf("%s\n", romNumeral);
+	// romNum: 
+	// tempNum: 4523
+	// idx: 4
+	// pValtemp: {1, -1, -1, -1}
 }
