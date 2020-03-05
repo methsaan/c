b92cf7fe69f1341b6278706a8be8ca327508993c
Methsaan <methsaan@gmail.com>
2020-03-05 17:52:44 -0500

    random schedule generator

---
+++ usefulprograms/schedGenerator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	char *activities[50];
+	char *tempAct;
+	int tempMin;
+	for (int x = 0; tempMin != -1; x++) {
+		
+	}
+}
