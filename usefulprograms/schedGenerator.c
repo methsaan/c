210d6bb8efbd56e6ce27ef463a7b64569721ab9a
Methsaan <methsaan@gmail.com>
2020-03-06 06:40:32 -0500

    day tracker

--- usefulprograms/schedGenerator.c
+++ usefulprograms/schedGenerator.c
@@ -2,10 +2,22 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	char *activities[50];
-	char *tempAct;
-	int tempMin;
-	for (int x = 0; tempMin != -1; x++) {
-		
+	int numOfReq;
+	int numOfOp;
+	printf("Enter number of required activities: ");
+	scanf("%d", &numOfReq);
+	printf("Enter number of optional activities: ");
+	scanf("%d", &numOfOp);
+	char *activitiesReq[numOfReq];
+	char *reqTime1[numOfReq];
+	char *reqTime2[numOfReq];
+	char *activitiesOp[numOfOp];
+	for (int x = 0; x < numOfOp; x++) {
+		printf("Enter optional activity %d: ", x+1);
+		scanf("%s", activitiesOp[x]);
+	}
+	for (int x = 0; x < numOfReq; x++) {
+		printf("Enter required activity %d beginning and end time: ", x+1);
+		scanf("%s %s %s", activitiesReq[x], reqTime1, reqTime2);
 	}
 }
