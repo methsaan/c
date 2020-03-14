5318e7bfbc5877346ee8337a7d64c85eb6c547cc
Methsaan <methsaan@gmail.com>
2020-03-14 08:46:54 -0400

    schedule flexibility update

--- usefulprograms/schedGenerator.c
+++ usefulprograms/schedGenerator.c
@@ -11,6 +11,11 @@ int main(int argc, char *argv[]) {
 	char *activitiesReq[numOfReq];
 	char *reqTime1[numOfReq];
 	char *reqTime2[numOfReq];
+	char *schedTime[2];
+	printf("Enter wake up time: ");
+	scanf("%s", schedTime[0]);
+	printf("Enter sleep time: ");
+	scanf("%s", schedTime[1]);
 	char *activitiesOp[numOfOp];
 	for (int x = 0; x < numOfOp; x++) {
 		printf("Enter optional activity %d: ", x+1);
@@ -18,6 +23,20 @@ int main(int argc, char *argv[]) {
 	}
 	for (int x = 0; x < numOfReq; x++) {
 		printf("Enter required activity %d beginning and end time: ", x+1);
-		scanf("%s %s %s", activitiesReq[x], reqTime1, reqTime2);
+		scanf("%s %s %s", activitiesReq[x], reqTime1[x], reqTime2[x]);
+	}
+	int reqTimeLen[numOfReq];
+	for (int x = 0; x < numOfReq; x++) {
+		int beginHour = reqTime1[x][0]-'0';
+		int beginMin = (reqTime1[x][2]-'0')*10 + (reqTime1[x][3]-'0');
+		int endHour = reqTime2[x][0]-'0';
+		int endMin = (reqTime2[x][2]-'0')*10 + (reqTime2[x][3]-'0');
+		int lenHour = endHour - beginHour;
+		int lenMin = endMin - beginMin;
+		if (lenMin < 0) {
+			lenHour -= 1;
+			lenMin += 60;
+		}
+		printf("%s: %d hours %d minutes\n", activitiesReq[x], lenHour, lenMin);
 	}
 }
