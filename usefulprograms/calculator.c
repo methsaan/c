ec861dc14f1cc10bfa7796a293bc9085adb5c3da
Methsaan <methsaan@gmail.com>
2020-03-21 17:36:47 -0400

    stack smash fix

--- usefulprograms/calculator.c
+++ usefulprograms/calculator.c
@@ -2,11 +2,10 @@
 #include <math.h>
 #include <string.h>
 
-int main(int argc, char* argv)
-{
+int main(int argc, char* argv) {
 	int x;
-	int y;
-	char op[3];
+	int y = 0;
+	char op[5];
 	printf("Enter operator:(add, sub, mul, div, sqrt) ");
 	scanf("%s", op);
 	printf("Enter x: ");
@@ -14,24 +13,21 @@ int main(int argc, char* argv)
 	if (strcmp(op, "sqrt") != 0) {
 		printf("Enter y: ");
 		scanf("%d", &y);
+	} else {
+		y = 0;
 	}
+	
 	if (strcmp(op, "add") == 0){
 		printf("%d + %d = %d\n", x, y, x+y);
-	}
-	else if (strcmp(op, "sub") == 0){
+	} else if (strcmp(op, "sub") == 0){
 		printf("%d - %d = %d\n", x, y, x-y);
-	}
-	else if (strcmp(op, "mul") == 0){
+	} else if (strcmp(op, "mul") == 0){
 		printf("%d \u00D7 %d = %d\n", x, y, x*y);
-	}
-	else if (strcmp(op, "div") == 0){
-		printf("%d \u00F7 %d = %f\n", x, y, (double)x/(double)y);
-	}
-	else if (strcmp(op, "sqrt") == 0){
-		double xFlt = x + 0.000000;
-		printf("%f\n", sqrt(x));
-	}
-	else {
-		printf("");
+	} else if (strcmp(op, "div") == 0){
+		printf("%d \u00F7 %d = %f\n", x, y, (double)x/y);
+	} else if (strcmp(op, "sqrt") == 0) {
+		printf("%f\n", sqrt(x) + y);
+	} else {
+		printf("Invalid operator\n");
 	}
 }
