94aa882118107db24e5e884daba2755fa3086fb0
Methsaan <methsaan@gmail.com>
2020-03-21 17:07:17 -0400

    directory

--- usefulprograms/argls.c
+++ usefulprograms/argls.c
@@ -4,6 +4,6 @@
 
 int main(int argc, char *argv[]) {
 	char *command = (char*)malloc(30 * sizeof(char));
-	sprintf(command, "ls %s %s*%s", argv[1], argc > 2 ? argv[2] : "", argc > 3 ? argv[3] : "");
+	sprintf(command, "ls %s/%s*%s", argv[1], argc > 2 ? argv[2] : "", argc > 3 ? argv[3] : "");
 	system(command);
 }
