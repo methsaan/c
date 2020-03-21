5a4f1902d466b14a4446134072d0329a36a827de
Methsaan <methsaan@gmail.com>
2020-03-21 17:53:05 -0400

    exec.c test

---
+++ usefulprograms/execfile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <ctype.h>
+
+int main(int argc, char *argv) {
+int y = 0;
+return 0;
+}
