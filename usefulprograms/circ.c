0f278161ee4a434ee843ce60802ba01e8571ecec
Methsaan <methsaan@gmail.com>
2020-11-27 20:43:31 -0500

    trailing newline

--- usefulprograms/circ.c
+++ usefulprograms/circ.c
@@ -9,47 +9,48 @@ int main(int argc, char *argv){
 	double height;
 	printf("Options: circle_area, circle_circ, cylinder_area, cylinder_volume, cone_area, cone_vol, sphere_area, sphere_vol\nEnter your option: ");
 	fgets(option, 100, stdin);
-	if (strcmp(option, "circle_area\n") == 0){
+	strtok(option, "\n");
+	if (strcmp(option, "circle_area") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Area: %.50fu\u00B2\n", PI*pow(radius, 2));
-	}else if (strcmp(option, "circle_circ\n") == 0){
+	}else if (strcmp(option, "circle_circ") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Circumference: %.50fu\n", 2*PI*radius);
-	}else if (strcmp(option, "cylinder_area\n") == 0){
+	}else if (strcmp(option, "cylinder_area") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Enter height: ");
 		scanf("%lf", &height);
 		printf("Surface area: %.50fu\u00B2\n", (PI*pow(radius, 2))*2+(2*PI*radius)*height);
-	}else if (strcmp(option, "cylinder_volume\n") == 0){
+	}else if (strcmp(option, "cylinder_volume") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Enter height: ");
 		scanf("%lf", &height);
 		printf("Volume: %.50fu\u00B3\n", (PI*pow(radius, 2))*height);
-	}else if (strcmp(option, "cone_area\n") == 0){
+	}else if (strcmp(option, "cone_area") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Enter height: ");
 		scanf("%lf", &height);
 		printf("Surface area: %.50fu\u00B2\n", PI*radius*(radius+sqrt(pow(radius, 2) + pow(height, 2))));
-	}else if (strcmp(option, "cone_vol\n") == 0){
+	}else if (strcmp(option, "cone_vol") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Enter height: ");
 		scanf("%lf", &height);
 		printf("Surface area: %.50fu\u00B3\n", (PI*pow(radius, 2)*height)/3);
-	}else if (strcmp(option, "sphere_area\n") == 0){
+	}else if (strcmp(option, "sphere_area") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
 		printf("Surface area: %.50fu\u00B2\n", 4*PI*pow(radius, 2));
-	}else if (strcmp(option, "sphere_vol\n") == 0){
+	}else if (strcmp(option, "sphere_vol") == 0) {
 		printf("Enter radius: ");
 		scanf("%lf", &radius);
-		printf("Volume: %.50fu\u00B3\n", (4.0/3)*(PI*pow(radius, 3)));
-	}else{
-		printf("%s: %.50f\n", option, 0.0);
+		printf("Volume: %.50fu\u00B3", (4.0/3)*(PI*pow(radius, 3)));
+	}else {
+		printf("%s: 0\n", option);
 	}
 }
