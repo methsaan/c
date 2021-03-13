a6e6f3ca65dcbdeedac02b99751ab250818a6993
Methsaan <methsaan@gmail.com>
2021-03-13 15:32:45 -0500

    write to file

--- usefulprograms/execrun.c
+++ usefulprograms/execrun.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFSIZE 128
+#include <errno.h>
+
+extern int errno;
 
 int main(int argc, char *argv) {
 	FILE *errorFile = fopen("errors.txt", "w");
 	fprintf(errorFile, "qweqweqwew");
 	fclose(errorFile);
-
-	char *cmd = "dqmpog";
-
-	char buf[BUFSIZE];
-	FILE *fp;
-
-	if ((fp = popen(cmd, "r")) == NULL) {
-		return -1;
-	}
-
-	while (fgets(buf, BUFSIZE, fp) != NULL) {
-		printf("%s", buf);
-	}
-
-	if (pclose(fp)) {
-		// error
-		printf("Command not found or exited with error status\n");
-		return -1;
-	}
 }
