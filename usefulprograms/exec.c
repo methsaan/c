d2d93eb7ae0ad1ca6b9f5577df0606bfb297c0c4
Methsaan <methsaan@gmail.com>
2021-05-26 21:05:16 -0400

    rewrite

--- usefulprograms/exec.c
+++ usefulprograms/exec.c
@@ -4,52 +4,19 @@
 
 int main(int argc, char *argv) {
 	FILE *fp;
-	FILE *fp2;
 	char code[500];
-	char origCode[500];
-	char declareCode[1000];
-	fp2 = fopen("execfile.c" , "r");
-	int c,nl=0,nc=0;
-	c=getc(fp2);
-	origCode[0] = c;
-	int idx = 1;
-	int lines = 0;
-	while(c!=EOF){
-		if(c=='\n')
-		nl++;
-		nc++;
-		c=getc(fp2);
-		origCode[idx] = c;
-		idx++;
-		if (c == '\n') {
-			lines++;
-		}
-		
-	}
 	int declareCodeIdx = 0;
+
 	while (strcmp(code, "return 0;\n") != 0) {
 		fp = fopen("execfile.c", "w+");
 		printf("Enter code: ");
 		fgets(code, 500, stdin);
-		int codeLen = 0;
 		for (int x = 0; code[x] != '\0'; x++) {
-			codeLen++;
+			declareCode[declareCodeIdx] = code[x];
+			declareCodeIdx++;
 		}
-		int hasDeclaration = 0;
-		for (int x = 0; x < codeLen; x++) {
-			if (code[x] == '=') {
-				hasDeclaration++;
-				break;
-			}
-		}
-		if (hasDeclaration == 1) {
-			for (int x = 0; code[x] != '\0'; x++){
-				declareCode[declareCodeIdx] = code[x];
-				declareCodeIdx++;
-			}
-			for (int x = 0; x < 500; x++) {
-				code[x] = '\0';
-			}
+		for (int x = 0; x < 500; x++) {
+			code[x] = '\0';
 		}
 		fprintf(fp, "#include <stdio.h>\n");
 		fprintf(fp, "#include <stdlib.h>\n");
@@ -71,13 +38,13 @@ int main(int argc, char *argv) {
 		system("./a.out");
 		system("cat errors.txt");
 		int size;
+
 		FILE *fp2 = fopen("errors.txt", "r");
-		printf("%s\n", declareCode);
 		if (fp2 != NULL) {
 			fseek(fp2, 0, SEEK_END);
 			size = ftell(fp2);
 			if (size == 0) {
-				system("head -n -2 execfile.c > tmp.txt && mv tmp.txt execfile.c");
+				
 			}
 		}
 		// doesn't work, delete line from code array
