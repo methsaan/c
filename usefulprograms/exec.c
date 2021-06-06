025b4c15541e12347f7093be443b28404994ef71
Methsaan <methsaan@gmail.com>
2021-06-06 17:51:55 -0400

    line numbers

--- usefulprograms/exec.c
+++ usefulprograms/exec.c
@@ -4,17 +4,22 @@
 
 int main(int argc, char *argv) {
 	FILE *fp;
-	char code[500];
-	int declareCodeIdx = 0;
+	char inputCode[50];
+	char code[1500];
+	int codeIdx = 0;
+	int lines[50];
+	int lineIdx = 0;
 
 	while (strcmp(code, "return 0;\n") != 0) {
-		fp = fopen("execfile.c", "w+");
-		printf("Enter code: ");
-		fgets(code, 500, stdin);
-		for (int x = 0; code[x] != '\0'; x++) {
-			declareCode[declareCodeIdx] = code[x];
-			declareCodeIdx++;
+		fp = fopen("execfile.c", "w");
+		printf("Enter command: ");
+		fgets(inputCode, 50, stdin);
+		for (int x = 0; inputCode[x] != '\0'; x++) {
+			code[codeIdx] = inputCode[x];
+			codeIdx++;
 		}
+		lines[lineIdx] = codeIdx;
+		lineIdx++;
 		for (int x = 0; x < 500; x++) {
 			code[x] = '\0';
 		}
@@ -25,8 +30,8 @@ int main(int argc, char *argv) {
 		fprintf(fp, "#include <stdbool.h>\n");
 		fprintf(fp, "#include <ctype.h>\n\n");
 		fprintf(fp, "int main(int argc, char *argv) {\n");
-		for (int x = 0; x < declareCodeIdx; x++) {
-			fprintf(fp, "%c", declareCode[x]);
+		for (int x = 0; x < codeIdx; x++) {
+			fprintf(fp, "%c", code[x]);
 		}
 		fprintf(fp, "%s", code);
 		fprintf(fp, "}\n");
@@ -47,7 +52,6 @@ int main(int argc, char *argv) {
 				
 			}
 		}
-		// doesn't work, delete line from code array
 		fclose(fp2);
 	}
 	system("gcc exec.c");
