78cb24250c77dd45dd5008ca507e43b2e062fa1d
Methsaan <methsaan@gmail.com>
2021-06-07 07:32:43 -0400

    store line lengths

--- usefulprograms/exec.c
+++ usefulprograms/exec.c
@@ -7,21 +7,21 @@ int main(int argc, char *argv) {
 	char inputCode[50];
 	char code[1500];
 	int codeIdx = 0;
-	int lines[50];
+	int lineLengths[50];
 	int lineIdx = 0;
 
 	while (strcmp(code, "return 0;\n") != 0) {
 		fp = fopen("execfile.c", "w");
 		printf("Enter command: ");
 		fgets(inputCode, 50, stdin);
-		for (int x = 0; inputCode[x] != '\0'; x++) {
+		for (int x = 0; inputCode[x-1] != '\n'; x++) {
 			code[codeIdx] = inputCode[x];
 			codeIdx++;
 		}
-		lines[lineIdx] = codeIdx;
+		lineLengths[lineIdx] = codeIdx;
 		lineIdx++;
-		for (int x = 0; x < 500; x++) {
-			code[x] = '\0';
+		for (int x = 0; x < 50; x++) {
+			inputCode[x] = '\0';
 		}
 		fprintf(fp, "#include <stdio.h>\n");
 		fprintf(fp, "#include <stdlib.h>\n");
@@ -33,26 +33,12 @@ int main(int argc, char *argv) {
 		for (int x = 0; x < codeIdx; x++) {
 			fprintf(fp, "%c", code[x]);
 		}
-		fprintf(fp, "%s", code);
 		fprintf(fp, "}\n");
 		fclose(fp);
-		char execute[800];
-		strcpy(execute, "");
-		char tempExecute[50];
+		// access execrun.h header
 		system("gcc execrun.c");
 		system("./a.out");
 		system("cat errors.txt");
-		int size;
-
-		FILE *fp2 = fopen("errors.txt", "r");
-		if (fp2 != NULL) {
-			fseek(fp2, 0, SEEK_END);
-			size = ftell(fp2);
-			if (size == 0) {
-				
-			}
-		}
-		fclose(fp2);
 	}
 	system("gcc exec.c");
 }
