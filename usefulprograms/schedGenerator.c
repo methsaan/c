a3cb059f7bcac1427b64d722dcebed2f210538b2
Methsaan <methsaan@gmail.com>
2021-10-23 19:15:59 -0400

    fixed items

--- usefulprograms/schedGenerator.c
+++ usefulprograms/schedGenerator.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int *argc, char *argv[]) {
-	size_t malloc_size = 100;
+	size_t malloc_size = 15;
+	size_t malloc_size2 = 30;
+	size_t malloc_size3 = 30;
 
 	char *requiredItems[10];
+	for (int x = 0; x < 10; x++) {
+		requiredItems[x] = malloc(malloc_size * sizeof(char));
+	}
+	int requiredItemMinutes[20];
 	char *fixedItems[20];
+	for (int x = 0; x < 20; x++) {
+		fixedItems[x] = malloc(malloc_size2 * sizeof(char));
+	}
+	int fixedItemMinutes[20];
 	char *customActivities[30];
+	for (int x = 0; x < 30; x++) {
+		fixedItems[x] = malloc(malloc_size3 * sizeof(char));
+	}
+	int customActivityMinutes[20];
+	
 
 	FILE *fp = fopen("requiredItems", "r");
 
-	requiredItems[0] = malloc(malloc_size * sizeof(char));
-
-	int eof = 0;
-	int n = 0;
-	while (fscanf(fp, "%99s", requiredItems[x]) != EOF) {
-		printf("haho %s\n", requiredItems[x]);
-		n++;
+	int riCnt = 0;
+	while (fscanf(fp, "%14s", requiredItems[riCnt]) != EOF) {
+		riCnt++;
 	}
 
-	for (int x = 0; x < n; x++) {
-		
+	int fiCnt = 0;
+	printf("Enter fixed items (enter q when done):\n");
+	do {
+		scanf("%29s", fixedItems[fiCnt]);
+		fiCnt++;
+	} while (fixedItems[fiCnt-1][0] != 'q');
+	for (int x = 0; x < fiCnt; x++) {
+		printf("%s\n", fixedItems[x]);
 	}
 
 	fclose(fp);
